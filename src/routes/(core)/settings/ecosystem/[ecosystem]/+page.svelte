9aa8dc9b2f45d8951fe8ae5b91f3ea678ae4e829
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-06-03 23:50:56 +0200

    Skeleton structure of the full app

---
+++ src/routes/(core)/settings/ecosystem/[ecosystem]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from "$app/stores";
+  import HeaderLine from "$lib/components/HeaderLine.svelte";
+
+  $: ecosystemName = $page.params.ecosystem
+</script>
+
+<HeaderLine title="{ecosystemName} settings"/>

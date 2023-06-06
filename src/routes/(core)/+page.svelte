4d22ae483c49b464e6717e4f081223fe8e056936
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-06-06 23:12:17 +0200

    Uniformize functions for home and sensors pages

--- src/routes/(core)/+page.svelte
+++ src/routes/(core)/+page.svelte
@@ -1,224 +1,273 @@
 <script>
-  import Fa from "svelte-fa";
-  import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+	import { onMount } from 'svelte';
 
-  import HeaderLine from "$lib/components/HeaderLine.svelte";
-  import Row from "$lib/components/layout/Row.svelte";
-  import Box from "$lib/components/layout/Box.svelte";
-  import BoxItem from "$lib/components/layout/BoxItem.svelte";
+	import Fa from 'svelte-fa';
+	import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
 
-  import {
-    currentUser,
-    ecosystems,
-    ecosystemsCurrentSensorsData, ecosystemsIds,
-    ecosystemsLight,
-    ecosystemsManagement,
-    serverCurrentData,
-    serverLastSeen,
-    serverLatency,
-    services,
-    warnings,
-    weatherCurrently,
-  } from "$lib/store.js";
-  import { permissions } from "$lib/utils/consts.js";
-  import {
-    computeEcosystemStatusClass,
-    computeLightingHours,
-    computeLightStatusClass,
-    computeCurrentSensorsAverages,
-    computeUptime,
-    isEmpty,
-    formatDate,
-    formatDateTime,
-    getWeatherIcon,
-    serviceEnabled
-  } from "$lib/utils/functions.js";
+	import HeaderLine from '$lib/components/HeaderLine.svelte';
+	import Row from '$lib/components/layout/Row.svelte';
+	import Box from '$lib/components/layout/Box.svelte';
+	import BoxItem from '$lib/components/layout/BoxItem.svelte';
 
-  export let data;
-  const {
-    ecosystemsCurrentSensorsDataValues, ecosystemsLightValues,
-    serverCurrentDataValues, serverStartTimeValue, weatherCurrentlyValues,
-  } = data;
-  ecosystemsCurrentSensorsData.set(ecosystemsCurrentSensorsDataValues);
-  ecosystemsLight.set(ecosystemsLightValues);
-  serverCurrentData.set(serverCurrentDataValues);
-  weatherCurrently.set(weatherCurrentlyValues)
+	import {
+		currentUser,
+		ecosystems,
+		ecosystemsIds,
+		ecosystemsManagement,
+		serverCurrentData,
+		ecosystemsSensorsSkeleton,
+		serverLastSeen,
+		serverLatency,
+		services,
+		warnings,
+		weatherCurrently
+	} from '$lib/store.js';
+	import { permissions } from '$lib/utils/consts.js';
+	import {
+		computeEcosystemStatusClass,
+		computeLightingHours,
+		computeLightStatusClass,
+		formatSensorsSkeleton,
+		computeUptime,
+		isEmpty,
+		formatDate,
+		formatDateTime,
+		getWeatherIcon,
+		serviceEnabled,
+		getParamStatus,
+		capitalize
+	} from '$lib/utils/functions.js';
+	import {
+		fetchSensorCurrentData,
+		fetchEcosystemsSensorsSkeleton,
+		fetchEcosystemLight,
+		fetchWeatherForecast
+	} from '$lib/actions.js';
 
-  let now = new Date();
-  const serverStartTime = new Date(serverStartTimeValue);
-  let filledBox = {};
+	export let data;
+	const { serverCurrentDataValues, serverStartTimeValue } = data;
+	serverCurrentData.set(serverCurrentDataValues);
+	const serverStartTime = new Date(serverStartTimeValue);
+	let now = new Date();
 
-  $: weatherEnabled = serviceEnabled($services, "weather");
-  $: calendarEnabled = serviceEnabled($services, "calendar");
-  $: uptime = computeUptime($serverLastSeen, serverStartTime)
+	const filledBox = {};
+	$: weatherEnabled = serviceEnabled($services, 'weather');
+	$: calendarEnabled = serviceEnabled($services, 'calendar');
+	$: uptime = computeUptime($serverLastSeen, serverStartTime);
+
+	const fetchAndComputeCurrentSensorsAverages = async function (FormattedSensorsSkeleton) {
+		if (isEmpty(FormattedSensorsSkeleton)) {
+			return [];
+		}
+		const average = (array) => array.reduce((a, b) => a + b) / array.length;
+		const rv = [];
+		for (const measureInfo of FormattedSensorsSkeleton) {
+			const sensors = measureInfo['sensors'];
+			if (isEmpty(sensors)) {
+				continue;
+			}
+			let values = [];
+			for (const sensorInfo of sensors) {
+				const data = await fetchSensorCurrentData(sensorInfo['uid'], measureInfo['measure']);
+				values.push(data['value']);
+			}
+			if (isEmpty(values)) {
+				continue;
+			}
+			rv.push({
+				measure: measureInfo['measure'],
+				unit: measureInfo['unit'],
+				average: average(values).toFixed(1)
+			});
+		}
+		return rv;
+	};
+
+	const formatCurrentSensorsAverages = function (averagedCurrentSensorsData) {
+		if (isEmpty(averagedCurrentSensorsData)) {
+			return '<p>No sensor data available</p>';
+		}
+		let rv = '';
+		for (const measureRecord of averagedCurrentSensorsData) {
+			rv += `<p>${
+				capitalize(measureRecord['measure']).replace('_', ' ') +
+				': ' +
+				measureRecord['average'] +
+				measureRecord['unit']
+			}</p>`;
+		}
+		return rv;
+	};
+
+	onMount(async () => {
+		const { weatherCurrentlyValues } = await fetchWeatherForecast('hourly,daily');
+		await fetchEcosystemsSensorsSkeleton(); // TODO: move to server ? But make sure not run too often
+		weatherCurrently.set(weatherCurrentlyValues);
+	});
 </script>
 
-<HeaderLine title={"Home"}/>
+<HeaderLine title={'Home'} />
 
 <h2>Global overview</h2>
 <Row>
-  <Box title="Calendar" align="center">
-    <BoxItem title={formatDate(now)}>
-      {#if calendarEnabled}
-        <p>Later here: your personal calendar</p>
-      {/if}
-    </BoxItem>
-  </Box>
-  {#if weatherEnabled && !isEmpty($weatherCurrently)}
-    <Box title="Current weather" align="center">
-      <i class="{ getWeatherIcon($weatherCurrently.icon) } weather-icon"></i>
-      <BoxItem title="{ $weatherCurrently.summary }">
-        <p>Temperature: { $weatherCurrently.temperature.toFixed(1) } °C</p>
-        <p>Wind: { $weatherCurrently.windSpeed.toFixed(1) } km/h</p>
-        <p>Precipitation:{ ($weatherCurrently.precipProbability * 100).toFixed(1) } %</p>
-        <p>Humidity: { ($weatherCurrently.humidity * 100).toFixed(1) } %</p>
-        <p>Cloud cover:{ ($weatherCurrently.cloudCover * 100).toFixed(1) } %</p>
-      </BoxItem>
-    </Box>
-  {/if}
-  {#if $currentUser.can(permissions.ADMIN) && !isEmpty($serverCurrentData)}
-    <Box title="Server info" align="center">
-      <BoxItem title="Uptime">
-        { uptime }
-      </BoxItem>
-      <BoxItem title="Average latency">
-        <p>{ $serverLatency } ms</p>
-      </BoxItem>
-      <BoxItem title="System usage">
-        <p>Average CPU load: { $serverCurrentData.CPU_used } %</p>
-        {#if $serverCurrentData.CPU_temp}
-          <p>CPU temperature: { $serverCurrentData.CPU_temp } °C</p>
-        {/if}
-        <p>RAM used:
-          { $serverCurrentData.RAM_used } GB / { $serverCurrentData.RAM_total } GB
-        </p>
-        <p>Disk used:
-          { $serverCurrentData.DISK_used } GB / { $serverCurrentData.DISK_total } GB
-        </p>
-      </BoxItem>
-    </Box>
-  {/if}
-  {#if $currentUser.isAuthenticated}
-    <Box title="Warnings overview" align="center">
-      <BoxItem>
-        {#each $warnings as warning}
-          { warning.content }
-        {:else}
-          No warning
-        {/each}
-      </BoxItem>
-    </Box>
-  {/if}
+	<Box title="Calendar" align="center">
+		<BoxItem title={formatDate(now)}>
+			{#if calendarEnabled}
+				<p>Later here: your personal calendar</p>
+			{/if}
+		</BoxItem>
+	</Box>
+	{#if weatherEnabled && !isEmpty($weatherCurrently)}
+		<Box title="Current weather" align="center">
+			<i class="{getWeatherIcon($weatherCurrently.icon)} weather-icon" />
+			<BoxItem title={$weatherCurrently.summary}>
+				<p>Temperature: {$weatherCurrently.temperature.toFixed(1)} °C</p>
+				<p>Wind: {$weatherCurrently.windSpeed.toFixed(1)} km/h</p>
+				<p>Precipitation:{($weatherCurrently.precipProbability * 100).toFixed(1)} %</p>
+				<p>Humidity: {($weatherCurrently.humidity * 100).toFixed(1)} %</p>
+				<p>Cloud cover:{($weatherCurrently.cloudCover * 100).toFixed(1)} %</p>
+			</BoxItem>
+		</Box>
+	{/if}
+	{#if $currentUser.can(permissions.ADMIN) && !isEmpty($serverCurrentData)}
+		<Box title="Server info" align="center">
+			<BoxItem title="Uptime">
+				{uptime}
+			</BoxItem>
+			<BoxItem title="Average latency">
+				<p>{$serverLatency} ms</p>
+			</BoxItem>
+			<BoxItem title="System usage">
+				<p>Average CPU load: {$serverCurrentData.CPU_used} %</p>
+				{#if $serverCurrentData.CPU_temp}
+					<p>CPU temperature: {$serverCurrentData.CPU_temp} °C</p>
+				{/if}
+				<p>
+					RAM used:
+					{$serverCurrentData.RAM_used} GB / {$serverCurrentData.RAM_total} GB
+				</p>
+				<p>
+					Disk used:
+					{$serverCurrentData.DISK_used} GB / {$serverCurrentData.DISK_total} GB
+				</p>
+			</BoxItem>
+		</Box>
+	{/if}
+	{#if $currentUser.isAuthenticated}
+		<Box title="Warnings overview" align="center">
+			<BoxItem>
+				{#each $warnings as warning}
+					{warning.content}
+				{:else}
+					No warning
+				{/each}
+			</BoxItem>
+		</Box>
+	{/if}
 </Row>
 
 {#if $ecosystemsIds.length > 0}
-  <h2>Ecosystems overview</h2>
-  {#each $ecosystemsIds as id}
-    <Box
-      title="{id.name}"
-      align="center"
-      status="{computeEcosystemStatusClass($ecosystems[id.uid])}"
-      direction="row"
-    >
-      <template>{filledBox[id.uid] = false}</template>
-      {#if
-        $ecosystems[id.uid] && $ecosystems[id.uid]['connected'] &&
-        $ecosystemsManagement[id.uid] && $ecosystemsManagement[id.uid]['light'] &&
-        $ecosystemsLight[id.uid]
-      }
-        <template>{filledBox[id.uid] = true}</template>
-        <BoxItem title="Light">
-          <p>
-            Status:
-            <Fa
-              icon={ faSyncAlt }
-              class="{computeLightStatusClass($ecosystemsLight[id.uid])}"
-              spin="{$ecosystemsLight[id.uid]['mode'] === 'automatic'}"
-            />
-          </p>
-          <p>{computeLightingHours($ecosystemsLight[id.uid])}</p>
-        </BoxItem>
-      {/if}
-      {#if
-        $ecosystems[id.uid] && $ecosystems[id.uid]['connected'] &&
-        $ecosystemsManagement[id.uid] && $ecosystemsManagement[id.uid]['environment_data']
-      }
-        <template>{filledBox[id.uid] = true}</template>
-        <BoxItem title="Environment">
-          {#if $ecosystemsCurrentSensorsData[id.uid]}
-            { @html computeCurrentSensorsAverages(
-                $ecosystemsCurrentSensorsData[id.uid],
-                'environment'
-              ) }
-          {:else}
-            <p>Collecting environment's data from the ecosystem</p>
-          {/if}
-        </BoxItem>
-      {/if}
-      {#if
-        $ecosystems[id.uid] && $ecosystems[id.uid]['connected'] &&
-        $ecosystemsManagement[id.uid] && $ecosystemsManagement[id.uid]['plants_data']
-      }
-        <template>{filledBox[id.uid] = true}</template>
-        <BoxItem title="Plants">
-          {#if $ecosystemsCurrentSensorsData[id.uid]}
-            { @html computeCurrentSensorsAverages(
-                $ecosystemsCurrentSensorsData[id.uid],
-                'plants'
-              ) }
-          {:else}
-            <p>Collecting plants data from the ecosystem</p>
-          {/if}
-        </BoxItem>
-      {/if}
-      {#if
-        $ecosystems[id.uid] && $ecosystems[id.uid]['connected']
-        && !$ecosystems[id.uid]["status"]
-      }
-        <template>{filledBox[id.uid] = true}</template>
-        <BoxItem>
-          <p>The ecosystem { id.name } is not currently running</p>
-          {#if $currentUser.can(permissions.OPERATE)}
-            <p>
-              Click
-              <a href="/settings/ecosystem/{id.name}">here</a>
-              to configure { id.name }
-            </p>
-         {/if}
-        </BoxItem>
-      {/if}
-      {#if $ecosystems[id.uid] && !$ecosystems[id.uid]["connected"]}
-        <template>{filledBox[id.uid] = true}</template>
-        <BoxItem>
-          <p>The ecosystem { id.name } is not currently connected</p>
-          <p>
-            Last connection to the server on
-            { formatDateTime(new Date($ecosystems[id.uid]["last_seen"])) }
-          </p>
-        </BoxItem>
-      {/if}
-      {#if $ecosystems[id.uid] && !filledBox[id.uid]}
-        <BoxItem>
-          <p>No functionality is enabled in { id.name }</p>
-          {#if $currentUser.can(permissions.OPERATE)}
-            <p>
-              Click
-              <a href="/settings/ecosystem/{id.name}">here</a>
-              to configure { id.name }
-            </p>
-          {/if}
-        </BoxItem>
-      {/if}
-    </Box>
-  {/each}
+	<h2>Ecosystems overview</h2>
+	{#each $ecosystemsIds as { uid, name }}
+		<Box
+			title={name}
+			align="center"
+			status={computeEcosystemStatusClass($ecosystems[uid])}
+			direction="row"
+		>
+			<template>{(filledBox[uid] = false)}</template>
+
+			{#if getParamStatus($ecosystems, uid, 'connected') && getParamStatus($ecosystemsManagement, uid, 'light')}
+				<template>{(filledBox[uid] = true)}</template>
+				<BoxItem title="Light">
+					{#await fetchEcosystemLight(uid)}
+						<p>Fetching data</p>
+					{:then ecosystemLight}
+						<p>
+							Status:
+							<Fa
+								icon={faSyncAlt}
+								class={computeLightStatusClass(ecosystemLight)}
+								spin={ecosystemLight['mode'] === 'automatic'}
+							/>
+						</p>
+						<p>{computeLightingHours(ecosystemLight)}</p>
+					{/await}
+				</BoxItem>
+			{/if}
+			{#if getParamStatus($ecosystems, uid, 'connected') && getParamStatus($ecosystemsManagement, uid, 'environment_data')}
+				<template>{(filledBox[uid] = true)}</template>
+				<BoxItem title="Environment">
+					{#await formatSensorsSkeleton($ecosystemsSensorsSkeleton, uid, 'environment')}
+						<p>Collecting environment's data from the ecosystem</p>
+					{:then skeleton}
+						{#await fetchAndComputeCurrentSensorsAverages(skeleton)}
+							<p>Collecting environment's data from the ecosystem</p>
+						{:then avg}
+							{@html formatCurrentSensorsAverages(avg)}
+						{/await}
+					{/await}
+				</BoxItem>
+			{/if}
+			{#if getParamStatus($ecosystems, uid, 'connected') && getParamStatus($ecosystemsManagement, uid, 'plants_data')}
+				<template>{(filledBox[uid] = true)}</template>
+				<BoxItem title="Plants">
+					{#await formatSensorsSkeleton($ecosystemsSensorsSkeleton, uid, 'plants')}
+						<p>Collecting environment's data from the ecosystem</p>
+					{:then skeleton}
+						{#await fetchAndComputeCurrentSensorsAverages(skeleton)}
+							<p>Collecting environment's data from the ecosystem</p>
+						{:then avg}
+							{@html formatCurrentSensorsAverages(avg)}
+						{/await}
+					{/await}
+				</BoxItem>
+			{/if}
+			{#if getParamStatus($ecosystems, uid, 'connected') && !$ecosystems[uid]['status']}
+				<template>{(filledBox[uid] = true)}</template>
+				<BoxItem>
+					<p>The ecosystem {name} is not currently running</p>
+					{#if $currentUser.can(permissions.OPERATE)}
+						<p>
+							Click
+							<a href="/settings/ecosystem/{name}">here</a>
+							to configure {name}
+						</p>
+					{/if}
+				</BoxItem>
+			{/if}
+			{#if $ecosystems[uid] && !$ecosystems[uid]['connected']}
+				<template>{(filledBox[uid] = true)}</template>
+				<BoxItem>
+					<p>The ecosystem {name} is not currently connected</p>
+					<p>
+						Last connection to the server on
+						{formatDateTime(new Date($ecosystems[uid]['last_seen']))}
+					</p>
+				</BoxItem>
+			{/if}
+			{#if $ecosystems[uid] && !filledBox[uid]}
+				<BoxItem>
+					<p>No functionality is enabled in {name}</p>
+					{#if $currentUser.can(permissions.OPERATE)}
+						<p>
+							Click
+							<a href="/settings/ecosystem/{name}">here</a>
+							to configure {name}
+						</p>
+					{/if}
+				</BoxItem>
+			{/if}
+		</Box>
+	{/each}
 {/if}
 
 <style>
-  .weather-icon {
-    height: 115px;
-    line-height: 115px;
-    font-size: 70px;
-    background: inherit;
-    margin-bottom: -1px;
-  }
+	.weather-icon {
+		height: 115px;
+		line-height: 115px;
+		font-size: 70px;
+		background: inherit;
+		margin-bottom: -1px;
+	}
 </style>

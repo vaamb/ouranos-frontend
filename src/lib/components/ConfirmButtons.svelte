a999c0fa883ecf9cccb09261c4658fd706598f8f
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-06-08 08:50:50 +0200

    Add a `ConfirmButtons` component
    - Useful for some modals

---
+++ src/lib/components/ConfirmButtons.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	export let disabled = false;
+
+	const dispatch = createEventDispatcher();
+
+	const cancel = function () {
+		dispatch('cancel');
+	};
+
+	const confirm = function () {
+		dispatch('confirm');
+	};
+</script>
+
+<div style="display: flex; justify-content: center; margin-top: 10px">
+	<button class="confirm" on:click={confirm} {disabled}> Confirm </button>
+	<button class="cancel" on:click={cancel}>Cancel</button>
+</div>
+
+<style>
+	button {
+		border: none;
+		color: white;
+		padding: 0.7em 1.4em;
+		margin: 0 25px;
+		text-align: center;
+		border-radius: 7px;
+		font-size: 1rem;
+		cursor: pointer;
+	}
+
+	button:disabled {
+		background-color: var(--derived-50);
+		cursor: not-allowed;
+	}
+
+	.confirm {
+		background-color: var(--green);
+	}
+
+	.cancel {
+		background-color: var(--red);
+	}
+</style>

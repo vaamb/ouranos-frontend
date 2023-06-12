f4fa4ceb9255576aaf3f9936226b456fd69e55c1
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-06-12 08:12:41 +0200

    Populate the switches page

--- src/routes/(core)/switches/[ecosystem]/+page.svelte
+++ src/routes/(core)/switches/[ecosystem]/+page.svelte
@@ -1,1 +1,35 @@
-WIP
+<script>
+	import { page } from '$app/stores';
+
+	import HeaderLine from '$lib/components/HeaderLine.svelte';
+	import Switch from '$lib/components/Switch.svelte';
+
+	import { fetchEcosystemRunningActuators, updateActuatorMode } from '$lib/actions.js';
+	import { ecosystemsIds } from '$lib/store.js';
+	import { actuatorType } from '$lib/utils/consts.js';
+	import { getEcosystemUid } from '$lib/utils/functions.js';
+
+	$: ecosystemName = $page['params']['ecosystem'];
+	$: ecosystemUID = getEcosystemUid($ecosystemsIds, ecosystemName);
+</script>
+
+<HeaderLine title="Actuator {ecosystemName}" />
+
+{#await fetchEcosystemRunningActuators(ecosystemUID) then runningActuators}
+	{#each actuatorType as actuator}
+		{#if runningActuators[actuator]['active']}
+			<Switch
+				actuatorType={actuator}
+				status={runningActuators[actuator]['status']}
+				mode={runningActuators[actuator]['mode']}
+				on:switch={(event) => {
+					updateActuatorMode(
+						ecosystemUID,
+						event['detail']['actuatorType'],
+						event['detail']['mode']
+					);
+				}}
+			/>
+		{/if}
+	{/each}
+{/await}

83c90bcd08235f22d022b8b8c03ab3a203a54d19
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-06-18 10:29:21 +0200

    Separate pure lighting information from actuators information

--- src/routes/(core)/switches/[ecosystem]/+page.svelte
+++ src/routes/(core)/switches/[ecosystem]/+page.svelte
@@ -4,7 +4,7 @@
 	import HeaderLine from '$lib/components/HeaderLine.svelte';
 	import Switch from '$lib/components/Switch.svelte';
 
-	import { fetchEcosystemRunningActuators, updateActuatorMode } from '$lib/actions.js';
+	import { fetchEcosystemActuatorsStatus, updateActuatorMode } from '$lib/actions.js';
 	import { ecosystemsIds } from '$lib/store.js';
 	import { actuatorType } from '$lib/utils/consts.js';
 	import { getEcosystemUid } from '$lib/utils/functions.js';
@@ -15,7 +15,7 @@
 
 <HeaderLine title="Actuator {ecosystemName}" />
 
-{#await fetchEcosystemRunningActuators(ecosystemUID) then runningActuators}
+{#await fetchEcosystemActuatorsStatus(ecosystemUID) then runningActuators}
 	{#each actuatorType as actuator}
 		{#if runningActuators[actuator]['active']}
 			<Switch

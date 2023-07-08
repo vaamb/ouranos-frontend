cd606d790c606c497789684bad5ee238f4fcb37c
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-07-08 20:22:58 +0200

    Update graphs when updating their values

--- src/lib/components/Graph.svelte
+++ src/lib/components/Graph.svelte
@@ -8,7 +8,7 @@
 	export let datasets;
 	export let suggestedMin = 0;
 	export let suggestedMax = 10;
-    export let height = 250;
+	export let height = 250;
 
 	let canvas;
 	let chart;
@@ -24,19 +24,27 @@
 			y: {
 				display: true,
 				suggestedMin: suggestedMin,
-				suggestedMax: suggestedMax,
-			},
+				suggestedMax: suggestedMax
+			}
 		},
 		plugins: {
 			legend: {
-				display: false,
+				display: false
 			}
 		},
 		pointRadius: 0,
 		pointHitRadius: 5,
 		maintainAspectRatio: false,
-		responsive: true,
+		responsive: true
 	};
+
+	$: {
+		if (chart) {
+			chart.data.labels = labels;
+			chart.data.datasets = datasets;
+			chart.update();
+		}
+	}
 
 	onMount(async () => {
 		const ctx = canvas.getContext('2d');
@@ -44,13 +52,13 @@
 			type: 'line',
 			data: {
 				labels: labels,
-				datasets: datasets,
+				datasets: datasets
 			},
-			options: chartLayout,
+			options: chartLayout
 		});
 	});
 </script>
 
-<div class="container" style="height:{ height }">
+<div class="container" style="height:{height}">
 	<canvas bind:this={canvas} width="clientWidth" height="clientHeight" />
 </div>

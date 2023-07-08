1d1286aae05f707e026011087a65e4a6e206da7c
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-07-08 17:23:44 +0200

    Uniformise current sensor data retrieval from SocketIO and api

--- src/lib/socketio.js
+++ src/lib/socketio.js
@@ -1,71 +1,100 @@
-import { Manager } from "socket.io-client";
+import { get } from 'svelte/store';
 
-import { base_URL } from "$lib/utils/consts.js";
+import { Manager } from 'socket.io-client';
+
+import { base_URL } from '$lib/utils/consts.js';
+import { getStoreDataKey, updateStoreData } from '$lib/utils/functions.js';
 import {
-  ecosystemsCurrentSensorsData,
-  serverCurrentData,
-  serverLastSeen,
-  serverLatency
-} from "$lib/store.js";
-
+	ecosystemsActuatorData,
+	ecosystemsSensorsDataCurrent,
+	ecosystemsLightData,
+	ecosystemsManagement,
+	serverCurrentData,
+	serverLastSeen,
+	serverLatency
+} from '$lib/store.js';
 
 let latencyArray = [];
 let pingTime = null;
 let pingLoop = null;
 
-
 const manager = new Manager(base_URL, {
-  autoConnect: false,
-  reconnectionDelayMax: 30000,
-  transports: ["websocket", "polling"]
+	autoConnect: false,
+	reconnectionDelayMax: 30000,
+	transports: ['websocket', 'polling']
 });
 
-const socketio = manager.socket("/");
+const socketio = manager.socket('/');
 
-export const connectSocketio = function() {
-  socketio.connect();
-}
+export const connectSocketio = function () {
+	socketio.connect();
+};
 
-export const disconnectSocketio = function() {
-  socketio.disconnect();
-}
+export const disconnectSocketio = function () {
+	socketio.disconnect();
+};
 
 const pingServer = function () {
-  pingTime = new Date();
-  socketio.emit("ping");
-}
+	pingTime = new Date();
+	socketio.emit('ping');
+};
 
 // Reserved events
-socketio.on("connect", (msg) => {
-  pingLoop = setInterval(pingServer, 3000);
-})
+socketio.on('connect', (msg) => {
+	pingLoop = setInterval(pingServer, 3000);
+});
 
-socketio.on("disconnect", (msg) => {
-  clearInterval(pingLoop);
-})
+socketio.on('disconnect', (msg) => {
+	clearInterval(pingLoop);
+});
 
 // Custom events
-socketio.on("pong", (msg) => {
-  const now = new Date();
-  serverLastSeen.set(now);
-  latencyArray.push(now - pingTime);
-  latencyArray = latencyArray.slice(-5);
-  let sum = 0;
-  for (let i = 0; i < latencyArray.length; i++) {
-    sum += latencyArray[i];
-  }
-  serverLatency.set(
-    (Math.round((10 * sum) / latencyArray.length) / 10).toFixed(1)
-  );
+socketio.on('pong', (msg) => {
+	const now = new Date();
+	serverLastSeen.set(now);
+	latencyArray.push(now - pingTime);
+	latencyArray = latencyArray.slice(-5);
+	let sum = 0;
+	for (let i = 0; i < latencyArray.length; i++) {
+		sum += latencyArray[i];
+	}
+	serverLatency.set((Math.round((10 * sum) / latencyArray.length) / 10).toFixed(1));
 });
 
 socketio.on("current_server_data", (data) => {
   serverCurrentData.set(data);
 });
 
-socketio.on("current_sensors_data", (data) => {
-  const object = data.reduce(
-    (a, v) => ({...a, [v["ecosystem_uid"]]: v}), {}
-  );
-  ecosystemsCurrentSensorsData.set(object);
+socketio.on('actuator_data', (data) => {
+	const updatedData = data.reduce((a, v) => ({ ...a, [v['uid']]: v['data'] }), {});
+	updateStoreData(ecosystemsActuatorData, updatedData);
+});
+
+socketio.on('current_sensors_data', (data) => {
+	const updatedData = {};
+	for (const ecosystem of data) {
+		if (!ecosystem['data']['records']) {
+			continue;
+		}
+		for (const sensorRecord of ecosystem['data']['records']) {
+			for (const measureRecord of sensorRecord['measures']) {
+				const storageKey = getStoreDataKey(sensorRecord['sensor_uid'], measureRecord['measure']);
+				updatedData[storageKey] = {
+					timestamp: new Date(ecosystem['data']['timestamp']),
+					value: measureRecord['value']
+				};
+			}
+		}
+	}
+	updateStoreData(ecosystemsSensorsDataCurrent, updatedData);
+});
+
+socketio.on('on_light_data', (data) => {
+	const updatedData = data.reduce((a, v) => ({ ...a, [v['uid']]: v['data'] }), {});
+	updateStoreData(ecosystemsLightData, updatedData);
+});
+
+socketio.on('on_management', (data) => {
+	const updatedData = data.reduce((a, v) => ({ ...a, [v['uid']]: v['data'] }), {});
+	updateStoreData(ecosystemsManagement, updatedData);
 });
--- src/lib/store.js
+++ src/lib/store.js
@@ -6,12 +6,12 @@ import { User } from "$lib/utils/factories.js";
 export const currentUser = cookieStore("userDataCache", User());
 export const ecosystems = writable({});
 export const ecosystemsIds = writable([]);
-export const ecosystemsCurrentSensorsData = writable({});
-export const ecosystemsLight = writable({});
+export const ecosystemsActuatorData = writable({});
+export const ecosystemsLightData = writable({});
 export const ecosystemsManagement = writable({});
-export const ecosystemsSensorsSkeleton = writable({})
 export const ecosystemsSensorsDataCurrent = writable({})
 export const ecosystemsSensorsDataHistoric = writable({})
+export const ecosystemsSensorsSkeleton = writable({})
 export const engines = writable({});
 export const enginesIds = writable([]);
 export const flashMessage = writable([]);

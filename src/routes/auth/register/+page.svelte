2f3177607ed1d661eb85b5d70e1785173b1b408e
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-01-29 23:26:30 +0100

    Fix auth-related pages (#22)
    * Add a `SERVER_URL` const for requests served from the server
    
    - This allows to reduce the time taken if the frontend and the backend api are on the same machine
    
    * Fix registration page
    
    - Create a new invitation page that ask for the token then redirect to the registration page
    
    - Email and passwords validity is indicated visually when written using the regex used for input validation
    
    * Reformat the `auth` routes with `Prettier` and fix styling discrepancies

--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -1,180 +1,195 @@
 <script>
-  import { goto } from "$app/navigation";
-  import { page } from "$app/stores";
+	import { page } from '$app/stores';
 
-  import axios from "axios";
-  import jwt_decode from "jwt-decode";
+	import axios from 'axios';
+	import jwt_decode from 'jwt-decode';
+	import Fa from 'svelte-fa';
+	import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
-  import {flashMessage} from "$lib/store.js";
-  import {API_URL} from "$lib/utils/consts.js";
-  import {Message} from "$lib/utils/factories.js";
+	import { flashMessage } from '$lib/store.js';
+	import { API_URL } from '$lib/utils/consts.js';
+	import { Message } from '$lib/utils/factories.js';
 
-  const rawToken = $page.url.searchParams.get("token");
-  let token;
-  let validToken;
-  let role;
+	let token = $page.url.searchParams.get('token');
 
-  try {
-    token = jwt_decode(rawToken);
-    validToken = true;
-    role = token.rle || "User";
-  } catch (error) {
-    token = null;
-    validToken = false;
-  }
+	let role;
+	try {
+		const data = jwt_decode(token);
+		role = data.rle || 'User';
+	} catch (error) {
+		token = null;
+	}
 
-  let invitationToken;
-  let invitationError = null;
+	const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+	const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-+_!$&?.,])[^ ]{8,20}$/;
 
-  const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-+_!$&?.,])(?=.{8,})[^ ]+$/
+	let errors = {};
+	const resetErrors = function () {
+		errors.email = null;
+		errors.password1 = null;
+		errors.password2 = null;
+		errors.server = null;
+	};
+	resetErrors();
 
-  const validateInvitation = function() {
-    invitationError = null;
-    try {
-      const decodedInvitationToken = jwt_decode(invitationToken);
-      if (new Date() >= decodedInvitationToken.exp * 1000) {
-        invitationError = "Expired token";
-      } else {
-        goto(`${API_URL}/auth/register?token=${invitationToken}`)
-      }
-    } catch (error) {
-      if (error.message) {
-        invitationError = "Invalid token";
-      }
-    }
-  }
+	let username;
+	let firstname;
+	let lastname;
+	let email;
+	let telegramId;
+	let password1;
+	let password2;
 
-  let errors = {};
-  const resetErrors = function() {
-    errors.email = null;
-    errors.password1 = null;
-    errors.password2 = null;
-    errors.server = null;
-  }
-  resetErrors()
+	const checkValidEmail = function (email) {
+		if (!email) {
+			return null;
+		}
+		return regexEmail.test(email);
+	};
 
-  let username;
-  let firstname;
-  let lastname;
-  let email;
-  let telegramId;
-  let password1;
-  let password2;
+	$: validEmail = checkValidEmail(email);
 
-  const validateRegistration = function() {
-    resetErrors();
-    if (! regexEmail.test(email)) {
-      errors.password1 = "Invalid email format."
-    }
-    if (! regexPassword.test(password2)) {
-      errors.password1 = "Invalid password format."
-    }
-    if (password1 !== password2) {
-      errors.password2 = "Both passwords should match."
-    }
-    axios
-      .post(`${API_URL}/auth/register?token=${token}`, {
-        username: username,
-        firstname: firstname,
-        lastname: lastname,
-        email: email,
-        telegram_id: telegramId,
-        password: password1,
-      })
-      .then(() => {
-        let msgs = $flashMessage;
-        msgs.push(Message("Hello " + username + ", welcome to Ouranos"))
-        flashMessage.set(msgs)
-      })
-      .catch((postError) => {
-        if (postError.response) {
-          if (postError.response.status === 400) {
-            errors.server = postError.response.data.detail
-          }
-        }
-      });
-  }
+	const checkValidPassword = function (password) {
+		if (!password) {
+			return null;
+		}
+		return regexPassword.test(password);
+	};
+
+	$: validPassword = checkValidPassword(password1);
+
+	const checkSamePassword = function (password1, password2) {
+		if (!password1 || !password2) {
+			return null;
+		}
+		return password1 === password2;
+	};
+
+	$: samePassword = checkSamePassword(password1, password2);
+
+	const validateRegistration = function () {
+		resetErrors();
+		if (!regexEmail.test(email)) {
+			errors.password1 = 'Invalid email format.';
+		}
+		if (!regexPassword.test(password2)) {
+			errors.password1 = 'Invalid password format.';
+		}
+		if (password1 !== password2) {
+			errors.password2 = 'Both passwords should match.';
+		}
+		axios
+			.post(`${API_URL}/auth/register?token=${token}`, {
+				username: username,
+				firstname: firstname,
+				lastname: lastname,
+				email: email,
+				telegram_id: telegramId,
+				password: password1
+			})
+			.then(() => {
+				let msgs = $flashMessage;
+				msgs.push(Message('Hello ' + username + ', welcome to Ouranos'));
+				flashMessage.set(msgs);
+			})
+			.catch((postError) => {
+				if (postError.response) {
+					if (postError.response.status === 400) {
+						errors.server = postError.response.data.detail;
+					}
+				}
+			});
+	};
+	const getValidationColorClass = function (validationCode) {
+		if (validationCode === null) {
+			return 'hidden';
+		} else if (validationCode === true) {
+			return 'on';
+		} else if (validationCode === false) {
+			return 'off';
+		}
+	};
 </script>
 
-{#if !rawToken}
-  <h1>Enter your registration token</h1>
-  <form on:submit={ validateInvitation }>
-    <div class="input-group">
-      {#if invitationError}
-        <div class="error">{ invitationError }</div>
-      {/if}
-      <input id="invitation" size="60" type="text" bind:value={ invitationToken } />
-    </div>
-    <div class="input-group">
-      <input id="submit-invitation" type="submit" class="submit-button" value="Validate" />
-    </div>
-  </form>
+{#if !token}
+	<p>Get a valid token and enter it <a href="/auth/invitation">here</a></p>
 {:else}
-  {#if !validToken}
-    <h1>Invalid token</h1>
-    <p>Get a new token and enter it <a href="/auth/register">here</a></p>
-  {:else}
-    <h1>Register</h1>
-    <form on:submit={ validateRegistration }>
-      {#if errors.server}
-        <div class="input-group">
-          {#each errors.server as error}
-            <div class="error">{ error }</div>
-          {/each}
-        </div>
-      {/if}
-      <div class="input-group">
-        <label for="username">Username</label> <br />
-        <input id="username" size="32" type="text" bind:value={ username } />
-        {#if errors.username}
-          <br />
-          <div class="error">{ errors.username }</div>
-        {/if}
-      </div>
-      <div class="input-group">
-        <label for="firstname">Firstname</label> <br />
-        <input id="firstname" size="32" type="text" bind:value={ firstname } />
-      </div>
-      <div class="input-group">
-        <label for="lastname">Lastname</label> <br />
-        <input id="lastname" size="32" type="text" bind:value={ lastname } />
-      </div>
-      <div class="input-group">
-        <label for="role">Role</label> <br />
-        <input id="role" size="32" type="text" value="{role}" disabled/>
-      </div>
-      <div class="input-group">
-        <label for="email">E-mail</label> <br />
-        <input id="email" size="32" type="text" bind:value={ email } />
-        {#if errors.email}
-          <br />
-          <div class="error">{ errors.email }</div>
-        {/if}
-      </div>
-      <div class="input-group">
-        <label for="telegram-id">Telegram ID</label> <br />
-        <input id="telegram-id" size="32" type="text" bind:value={ telegramId } />
-      </div>
-      <div class="input-group">
-        <label for="password1">Password</label> <br />
-        <input
-          id="password1" size="32" type="text" bind:value={ password1 }
-          placeholder="Should contain at least one lowercase, one capital, one number and one special character"
-        />
-        {#if errors.password1}
-          <br />
-          <div class="error">{ errors.password1 }</div>
-        {/if}
-      </div>
-      <div class="input-group">
-        <label for="password2">Repeat your password</label> <br />
-        <input id="password2" size="32" type="text" bind:value={ password2 } />
-        {#if errors.password2}
-          <br />
-          <div class="error">{ errors.password2 }</div>
-        {/if}
-      </div>
-    </form>
-  {/if}
+	<h1>Register</h1>
+	<form on:submit={validateRegistration}>
+		{#if errors.server}
+			<div class="input-group">
+				{#each errors.server as error}
+					<div class="error">{error}</div>
+				{/each}
+			</div>
+		{/if}
+		<div class="input-group">
+			<label for="username">Username</label> <br />
+			<input id="username" size="32" type="text" bind:value={username} />
+			{#if errors.username}
+				<br />
+				<div class="error">{errors.username}</div>
+			{/if}
+		</div>
+		<div class="input-group">
+			<label for="firstname">Firstname</label> <br />
+			<input id="firstname" size="32" type="text" bind:value={firstname} />
+		</div>
+		<div class="input-group">
+			<label for="lastname">Lastname</label> <br />
+			<input id="lastname" size="32" type="text" bind:value={lastname} />
+		</div>
+		<div class="input-group">
+			<label for="role">Role</label> <br />
+			<input id="role" size="32" type="text" value={role} disabled />
+		</div>
+		<div class="input-group">
+			<label for="email">E-mail</label> <br />
+			<input id="email" size="32" type="text" bind:value={email} />
+			<Fa icon={faCircle} class={getValidationColorClass(validEmail)} />
+			{#if errors.email}
+				<br />
+				<div class="error">{errors.email}</div>
+			{/if}
+		</div>
+		<!--
+		<div class="input-group">
+			<label for="telegram-id">Telegram ID</label> <br />
+			<input id="telegram-id" size="32" type="text" bind:value={telegramId} />
+		</div>
+		-->
+		<div class="input-group">
+			<label for="password1">Password</label> <br />
+			<input id="password1" size="32" type="password" bind:value={password1} />
+			<div style="display: {validPassword !== null ? 'auto' : 'none'}" />
+			<Fa icon={faCircle} class={getValidationColorClass(validPassword)} />
+			{#if errors.password1}
+				<br />
+				<div class="error">{errors.password1}</div>
+			{:else}
+				<p style="max-width: 250px; margin: 0; font-size: smaller">
+					Should be between 8 and 20 characters long, contain at least one lower case letter, one
+					capital letter, one number and one special character amongst -+_!$&?.,
+				</p>
+			{/if}
+		</div>
+		<div class="input-group">
+			<label for="password2">Repeat your password</label> <br />
+			<input id="password2" size="32" type="password" bind:value={password2} />
+			<Fa icon={faCircle} class={getValidationColorClass(samePassword)} />
+			{#if errors.password2}
+				<br />
+				<div class="error">{errors.password2}</div>
+			{/if}
+		</div>
+		<div class="input-group">
+			<input
+				id="submit-invitation"
+				type="submit"
+				class="submit-button"
+				value="Validate"
+				style="height: 2rem"
+			/>
+		</div>
+	</form>
 {/if}

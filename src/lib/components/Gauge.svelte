77c2fb7c251341262c1c7f1a8cc3b92a71ed4770
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-01-29 20:08:30 +0100

    Update gauge (#2)
    * More coherent `Gauge` structure
    
    - Prettier format
    
    * Better handling of `Gauge` style on smartphone
    
    * Better handle `undefined` min and max values params passed to `Gauge`
    
    * Softer gauge limits

--- src/lib/components/Gauge.svelte
+++ src/lib/components/Gauge.svelte
@@ -1,56 +1,72 @@
 <script>
-  import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-  import { Gauge } from "gaugeJS";
+	import { Gauge } from 'gaugeJS';
 
-  import { gaugeOpts } from "$lib/utils/styling.js";
+	export let defaultMin = 0;
+	export let value = 50;
+	export let minValue = defaultMin;
+	export let defaultMax = 100;
+	export let maxValue = defaultMax;
+	export let unit = '';
 
-  export let minValue = 0;
-  export let value = 50;
-  export let maxValue = 100;
-  export let unit = "";
+	let canvas;
+	let gauge;
 
-  let canvas;
-  let gauge
+	const gaugeOpts = {
+		lines: 12,
+		angle: -0.11, // The span of the gauge arc
+		lineWidth: 0.25, // The line thickness
+		radiusScale: 0.7, // Relative radius
+		pointer: {
+			length: 0.6, // Relative to gauge radius
+			strokeWidth: 0.035, // The thickness
+			color: '#000000' // Fill color
+		},
+		limitMax: false, // If false, max value increases automatically if value > maxValue
+		limitMin: false, // If true, the min value of the gauge will be fixed
+		colorStart: '#6FADCF', // Colors
+		colorStop: '#8FC0DA', // just experiment with them
+		strokeColor: '#E0E0E0', // to see which ones work best for you
+		generateGradient: true,
+		highDpiSupport: true // High resolution support
+	};
 
-  $: {
-    if (gauge) {
-      gauge.set(value)
-    }
-  }
+	$: {
+		if (gauge) {
+			gauge.set(value);
+		}
+	}
 
-  onMount(async () => {
-    gauge = new Gauge(canvas)
-    gauge.setOptions(gaugeOpts); // create sexy gauge!
-    gauge.maxValue = maxValue; // set max gauge value
-    gauge.minValue = minValue;  // set min value
-    gauge.set(value);
-  })
-
-
+	onMount(async () => {
+		gauge = new Gauge(canvas);
+		gauge.setOptions(gaugeOpts); // create sexy gauge!
+		gauge.minValue = minValue || defaultMin; // set min value
+		gauge.maxValue = maxValue || defaultMax; // set max gauge value
+		gauge.set(value);
+	});
 </script>
 
 <div class="container">
-  <div>
-    <canvas bind:this={canvas} width="clientWidth" height="clientHeight"></canvas>
-  </div>
+	<canvas bind:this={canvas} />
 
-  <span class="value">{value} {unit}</span>
+	<span class="value">{value} {unit}</span>
 </div>
 
 <style>
-  canvas {
-    width: 100%;
-    height: 100%;
-    margin-top: -15px;
-  }
+	canvas {
+		width: 100%;
+		height: 100%;
+		margin: auto;
+		max-width: 300px;
+	}
 
-  .container {
-    display: flex;
-    flex-direction: column;
-  }
+	.container {
+		display: flex;
+		flex-direction: column;
+	}
 
-  .value {
-    font-size: 1.4rem;
-  }
+	.value {
+		font-size: 1.2rem;
+	}
 </style>

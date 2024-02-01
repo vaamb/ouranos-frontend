808160ffb5bdec1bf530bd431eae666b097d69dc
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-02-01 18:35:20 +0100

    Improve python wrapper (#28)
    * Allow to set address and ports in development and non-dev builds
    
    * Fix async issues
    
    * Make locating the frontend directory more resilient

--- python_wrapper/ouranos_frontend/config.py
+++ python_wrapper/ouranos_frontend/config.py
@@ -1,3 +1,4 @@
 class Config:
+    FRONTEND_USE_SSL: bool = False
     FRONTEND_ADDRESS: str = "127.0.0.1"
     FRONTEND_PORT: int = 3000
--- python_wrapper/ouranos_frontend/main.py
+++ python_wrapper/ouranos_frontend/main.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+from pathlib import Path
 import subprocess
 
 from ouranos import current_app
@@ -13,19 +15,43 @@ class Frontend(Functionality):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.subprocess: subprocess.Popen | None = None
-        base_dir = get_base_dir().absolute()
-        self.frontend_dir = base_dir/"lib/ouranos-frontend"
+        self.frontend_dir = self._get_frontend_dir().absolute()
 
-    def _startup(self):
+    @staticmethod
+    def _get_frontend_dir() -> Path:
+        # Check if the env var "OURANOS_FRONTEND_DIR" should be used
+        frontend_dir_var = os.environ.get("OURANOS_FRONTEND_DIR")
+        if frontend_dir_var is not None:
+            frontend_dir = Path(frontend_dir_var)
+            if frontend_dir.exists():
+                return frontend_dir
+        # Try to use the default case
+        base_dir = get_base_dir()
+        frontend_dir = base_dir / "lib/ouranos-frontend"
+        if frontend_dir.exists():
+            return frontend_dir
+        # Try to find the dir from the package
+        this_dir = Path(__file__).absolute().parent
+        frontend_dir = this_dir.parents[1]
+        if frontend_dir.exists():
+            return frontend_dir
+        raise ValueError("Cannot find the frontend end directory")
+
+    async def _startup(self):
+        address = current_app.config.get("FRONTEND_ADDRESS", Config.FRONTEND_ADDRESS)
+        port = current_app.config.get("FRONTEND_PORT", Config.FRONTEND_PORT)
         if current_app.config["DEVELOPMENT"]:
             cmd = [
                 "npm",
                 "run",
                 "dev",
                 "--prefix", str(self.frontend_dir),
+                "--",
+                "--host", str(address),
+                "--port", str(port),
             ]
             self.logger.info(
-                "Vite development server running on http://127.0.0.1:5173 "
+                f"Vite development server running on http://{address}:{port} "
                 "(Press CTRL+C to quit)"
             )
         else:
@@ -34,13 +60,16 @@ class Frontend(Functionality):
             cmd = [
                 "node",
                 str(self.frontend_dir/"build"),
-                ]
+                "--",
+                "--host", str(address),
+                "--port", str(port),
+            ]
             self.logger.info(
                 f"Node running on http://{address}:{port} (Press CTRL+C to quit)"
             )
         self.subprocess = subprocess.Popen(
             cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
 
-    def _shutdown(self):
+    async def _shutdown(self):
         self.subprocess.terminate()
         self.subprocess = None

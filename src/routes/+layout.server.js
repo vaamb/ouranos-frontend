0282d183dc59146a895e11c7b567db4b1b207a9a
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-03-06 22:44:50 +0100

    Only make a request to get the current user info if a session cookie is found (#37)
    * Only make a request to get the current user info if a session cookie is found
    
    * Use Prettier on the root `+layout.server.js`

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,34 +1,36 @@
-import {error} from "@sveltejs/kit";
+import { error } from '@sveltejs/kit';
 
-import { APP_MODE, AppMode } from "../conf.js";
-import { fetchCurrentUserData, fetchServerInfo } from "$lib/actions.js";
+import { APP_MODE, AppMode } from '../conf.js';
+import { fetchCurrentUserData, fetchServerInfo } from '$lib/actions.js';
 
 export async function load({ cookies, request }) {
-  const rv = {
-    development: APP_MODE === AppMode.development,
-  }
-  const {appVersion, serverStatus} = await fetchServerInfo();
-  rv.appVersion = appVersion;
-  rv.serverStatus = serverStatus;
+	const rv = {
+		development: APP_MODE === AppMode.development
+	};
+	const { appVersion, serverStatus } = await fetchServerInfo();
+	rv.appVersion = appVersion;
+	rv.serverStatus = serverStatus;
 
-  const clientSessionCookie = "session=" + cookies.get("session")
-  const clientUserAgent = request.headers.get("user-agent")
-  const { currentUserData } = await fetchCurrentUserData(clientSessionCookie, clientUserAgent)
-  rv.userData = currentUserData
+	const sessionCookie = cookies.get('session');
+	if (sessionCookie !== undefined) {
+		const clientSessionCookie = 'session=' + sessionCookie;
+		const clientUserAgent = request.headers.get('user-agent');
+		const { currentUserData } = await fetchCurrentUserData(clientSessionCookie, clientUserAgent);
+		rv.userData = currentUserData;
+	} else {
+		rv.userData = undefined;
+	}
 
-  switch (APP_MODE) {
-  case AppMode.development:
-  case AppMode.testing:
-  case AppMode.production:
-    rv.maintenance = false;
-    return rv;
-  case AppMode.maintenance:
-    rv.maintenance = true;
-    return rv;
-  default:
-    error(
-      500,
-      "We are experiencing an unexpected issue, come back later"
-    );
-  }
+	switch (APP_MODE) {
+		case AppMode.development:
+		case AppMode.testing:
+		case AppMode.production:
+			rv.maintenance = false;
+			return rv;
+		case AppMode.maintenance:
+			rv.maintenance = true;
+			return rv;
+		default:
+			error(500, 'We are experiencing an unexpected issue, come back later');
+	}
 }

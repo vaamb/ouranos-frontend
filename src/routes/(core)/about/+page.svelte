e09846437a3ed65f28a0e069c9bc8b3ccf899adc
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-03-06 23:15:48 +0100

    Rewrite the about page

--- src/routes/(core)/about/+page.svelte
+++ src/routes/(core)/about/+page.svelte
@@ -6,71 +6,88 @@
 
 <div class="text">
 	<p>
-		GAIA stands for the Greenhouse Automation Intuitive App. It is a side project started during my PhD study in
-        plants biology to replicate the functionalities of the phytotrons (growth chambers) used in the lab. It started
-        as a small script written in Python running on a Raspberry Pi Zero. It was a simple loop that would check if
-        the lights were on or off, read the temperature and humidity from a DHT22 sensor and write it in a text file.
+        GAIA, short for Greenhouse Automation Intuitive App, is a side project I started during my doctoral research in
+        plant biology, aiming to emulate the functionalities of laboratory phytotrons (growth chambers).
+        It was initially conceived as a small Python script, operating on a Raspberry Pi Zero, executing a basic
+        routine.
+        This routine involved monitoring the status of lights, capturing temperature and humidity data through a DHT22
+        sensor, and logging the information into a text file.
 	</p>
 	<p>
-		Then, as I started to learn a bit more to code with  Python, I wanted to add some functionalities. First was
-        the possibility to read the configuration from a YAML file instead of having to write it in the main Python
-        script. Then, as the text file used to record the sensor data quickly became impractical, I wanted to log those
-        data into a database (using MySQLdb and SQL queries).
+		Then, as I delved deeper into Python, I wanted to add some functionalities. First, I focused on incorporating
+        the possibility to extract configurations from a YAML file, streamlining the process instead of embedding them
+        directly into the main Python script.
+        Then, as I became limited by possibilities offered by storing my sensor data in a text file, I sought to
+        transition to a more efficient method by logging them into a database.
+        This involved utilizing sqlite and executing SQL queries for data management.
 	</p>
 	<p>
-		Now, those data were more easy to search through but I wanted to be able to visualise them. To do so, I used
-        Flask to build a simple website and added a bit of javascript with chartjs to display simple graphs. As the
-        project grew, it became more and more resources hungry on my poor Pi Zero and I choose to divide GAIA in two
-        sub-programs and to migrate them to a new Pi 3B+. Those news programs were gaiaEngine (that became
-        <a href="https://github.com/vaamb/gaia">Gaia</a>, without capital letters), and gaiaWeb (that became
-        <a href="https://github.com/vaamb/ouranos-core">Ouranos</a>, the husband of Gaia in the greek mythology).
-        gaiaEngine was composed of the functionalities that took care of the plants while gaiaWeb was the Flask server.
+        As the volume of data increased, I soon wanted to  incorporate visualization capabilities.
+        To achieve this, I used Flask to construct a small website and integrated JavaScript with Chart.js to render
+        basic graphs.
+        However, with the project's expansion, it began to strain the resources of my poor Raspberry Pi Zero.
+        Consequently, I chose to split GAIA into two distinct sub-programs and migrate them to a more capable Pi 3B+.
+        These programs were gaiaEngine (renamed <a href="https://github.com/vaamb/gaia">Gaia</a>, in lowercase),
+        responsible for managing plant-related functionalities, and gaiaWeb (that became
+        <a href="https://github.com/vaamb/ouranos-core">Ouranos</a>, after the husband of Gaia in Greek mythology),
+        serving as the Flask server.
     </p>
     <p>
-        As I followed Flask guides made by Miguel Grinberg, gaiaWeb architecture totally changed and became more
-        structured and I started to use an ORM (SQLAlchemy) to spend less time on SQL and more on Python. In parallel,
-        I wanted Gaia to be more flexible so I created sensors templates as I learnt how to use Python classes. I also
-        realised that while the web server should be operated on the Raspberry Pi 3B+, I could still run Gaia (which I
-        tried to keep relatively light) on my old Pi Zero but I needed to share the data between Gaia and Ouranos
-        without relying on the fact that they were launched in the same interpreter and thus shared their memory.
+        As I immersed myself in Flask tutorials, the architecture of gaiaWeb underwent a significant transformation,
+        evolving into a more organized structure.
+        One of the changes undertaken at that time is that I started to use an ORM (SQLAlchemy) to streamline database
+        operations, allowing me to allocate more time to Python development rather than writing SQL queries.
+        In parallel, I wanted to increase Gaia's flexibility, thus, I started to create sensor templates as I learnt
+        more about OOP and how to use Python classes.
+        I also realised that while the web server had to be operated on the Raspberry Pi 3B+, I could still use Gaia
+        (which I tried to keep relatively light) on my old Pi Zero. However, I faced the challenge of synchronizing
+        data between Gaia and Ouranos without relying on shared memory, necessitating a more sophisticated approach.
     </p>
     <p>
-        The first step was to totally isolate Gaia from Ouranos. This was a task harder than I thought as many Python
-        objects were shared between the two sub programs. Then I looked how to share data between Python processes not
-        running in the same interpreter and not even running on the same computer. In the end, I choose to use Socket.IO
-        to share data between Gaia and Ouranos as I had used to update the data on my graphs in real time and it was
-        doing a rather good job.
+        The first step was to totally isolate Gaia from Ouranos.
+        This proved to be a more challenging task than anticipated, given the numerous Python objects shared between
+        the two subsystems.
+        To address this, I explored methods for sharing data between Python processes operating across different
+        interpreters and even different machines.
+        Ultimately, I opted to use Socket.IO for data sharing between Gaia and Ouranos.
+        This decision was influenced by my prior success in using Socket.IO for real-time data updates in graph
+        visualization, where it was doing a rather good job.
 	</p>
 	<p>
-		This worked for a while, but I still wanted to add even more functionalities to the server and realised that
-        while it could be useful to instantiate multiple Flask processes in the future, some new functionalities (such
-        as a database archiver to store old db entries to a long term storage db, a script to get the weather, another
-        to get the sunrise and sunset times, ...) should be launched only a single time. There again, the need to share
-        data between multiple processes arose. However, this time I used a new approach and wrote a simple
-        <a href="https://github.com/vaamb/event-dispatcher">event dispatcher</a> with an API comparable to the one of
-        Socket.IO. I tried to make this dispatcher broker agnostic and it can run in memory, using Redis or RabbitMQ,
-        and it is easy to add new brokers.
+		This worked for a while, but I wanted to to expand the server's capabilities further.
+        I realized that while it might be beneficial to instantiate multiple Flask processes down the line, certain new
+        functionalities (such as a database archiver for storing old entries in a long-term storage database, a script
+        for retrieving weather data, and another for fetching sunrise and sunset times) should only be launched once.
+        This raised the issue of sharing data across multiple processes once again.
+        However, this time I took a different approach and developed a basic
+        <a href="https://github.com/vaamb/event-dispatcher">event dispatcher</a> with an API similar to Socket.IO's.
+        I aimed to make this dispatcher independent of any specific messaging broker, enabling versatility.
+        It can operate in-memory, utilize Redis or RabbitMQ, and adding support for additional brokers is
+        straightforward.
 	</p>
 	<p>
-		At the same time, as I was making the backend more resilient, I was adding interactivity to the frontend (still
-        based on Flask and Jinja2) by writing more and more code in Javascript and using the possibilities offered by
-        Socket.IO. However, at one point, I started to have too much spaghetti code and I looked for a solution. This
-        solution was to use a Javascript frontend framework and I chose Vue.js. Here again, a big modification was
-        needed on Ouranos: change all the Flask routes I had been working on and change them into an API, and port all
-        the templates that I did using Jinja2 into Vue.js.
+        Simultaneously, while enhancing the backend's resilience, I was continuing to add interactivity to the frontend,
+        leveraging JavaScript and the capabilities of Socket.IO, while still relying on Flask and Jinja2.
+        However, as the codebase grew, I found myself grappling with spaghetti code, prompting me to seek a solution.
+        Enter Vue.js, a JavaScript frontend framework.
+        This decision necessitated significant modifications to Ouranos: transitioning all the Flask routes I had been
+        working on into an API structure and migrating Jinja2-based templates to Vue.js components.
 	</p>
 	<p>
-        At that time, my thesis came to its end and I had less free time to play with GAIA. When I passed it, I started
-        again to play with GAIA. Soon, I found Vue too "boilerplate-y" and I missed the possibility to validate
-        the queries Flask received. That's how I once again switched not one, but two of the frameworks I was using. For
-        Ouranos backend, I chose FastAPI, thanks to which I finally had to learn asyncio and discovered Pydantic, which
-        I sprinkled everywhere some input was needed, both in Ouranos and Gaia. For the frontend, I chose Svelte (and
-        later SvelteKit) that finally made Javascript a bit less painful to write.
+        During the final stages of my thesis, I had limited free time to play with GAIA.
+        However, once I completed it, I resumed my experiments with GAIA.
+        Quickly, I found Vue.js to be overly "boilerplate-y" and yearned for the ability to validate queries received by
+        Flask.
+        This led me to switch not just one, but two of the frameworks I had been using.
+        For the backend of Ouranos, I opted for FastAPI, which introduced me to asyncio and Pydantic.
+        Pydantic became integral for validating inputs across both Ouranos and Gaia.
+        For the <a href="https://github.com/vaamb/ouranos-frontend">frontend</a>, I chose Svelte (and later SvelteKit),
+        which made Javascript a bit less painful to write.
     </p>
 	<p>
-		Finally, here is the (temporary) result (until I decide to change another thing), a side project I had a lot of
-        fun to play with and thanks to which I learnt a lot about Python, project architecture, databases, Javascript,
-        NGINX, concurrency, RabbitMQ ...
+        Here is the current snapshot of my side project, until I decide to make further changes.
+        I've thoroughly enjoyed delving into this project, as it has provided me with valuable insights into Python,
+        project architecture, databases, JavaScript, NGINX, concurrency, RabbitMQ, and more ...
 	</p>
 </div>
 

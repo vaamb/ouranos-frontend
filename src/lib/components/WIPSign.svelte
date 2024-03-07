9e64932c14ecae30d759198c24f564896a4c42ee
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2024-03-07 11:09:42 +0100

    Add a "Work In Progress" component (#45)
    * Make sure "main" div span the whole space available
    
    * Run Prettier on `app.html`
    
    * Add a `WIPSign` component and use it instead of the plain "WIP" text

---
+++ src/lib/components/WIPSign.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Fa from 'svelte-fa';
+	import { faPersonDigging } from '@fortawesome/free-solid-svg-icons';
+</script>
+
+<div style="display: flex; height: 100%">
+	<div style="margin: auto; display: flex; flex-direction: column">
+		<div style="margin: auto; margin-bottom: 1rem; color: var(--graph-yellow)">
+			<Fa icon={faPersonDigging} size="5x" />
+		</div>
+		<p style="margin-bottom: 0; font-size: 1.5rem; font-weight: 550">Work In Progress</p>
+	</div>
+</div>

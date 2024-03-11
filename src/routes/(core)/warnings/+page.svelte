3c8ab56d687a486a3e6803d9e77924dccda009cc
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-03-12 00:56:22 +0100

    Populate the warnings page

--- src/routes/(core)/warnings/+page.svelte
+++ src/routes/(core)/warnings/+page.svelte
@@ -1,5 +1,62 @@
 <script>
-    import WIPPage from "$lib/components/WIPSign.svelte";
+	import { goto } from '$app/navigation';
+
+    import HeaderLine from "$lib/components/HeaderLine.svelte";
+    import Modal from "$lib/components/Modal.svelte";
+    import Table from "$lib/components/Table.svelte";
+
+    import {crudRequest} from "$lib/actions.js";
+    import { currentUser, warnings } from '$lib/store.js';
+
+	if (!$currentUser.isAuthenticated) {
+		goto('/');
+	}
+
+    // Crud-related variables and functions
+    let closeModal;
+	let crudDataIndex = null;
+
+	const setCrudData = function (rowIndex) {
+		crudDataIndex = rowIndex !== undefined ? rowIndex: null;
+	};
+
+	const resetCrudData = function () {
+		crudDataIndex = null;
+	};
 </script>
 
-<WIPPage />
+<HeaderLine title="Ecosystem warnings" />
+
+{#if $warnings.length > 0}
+    <Table
+        tableID="warnings"
+        columns={[
+            { label: 'Level', key: 'level' },
+            { label: 'Title', key: 'title' },
+            { label: 'Description', key: 'description' },
+            { label: 'Created on', key: 'created_on', isTime: true }
+        ]}
+        data={$warnings}
+        editable={true}
+        crudOptions={['delete']}
+        on:crud={(event) => {
+			setCrudData(
+				event['detail']['rowIndex']
+			);
+		}}
+    />
+    <Modal
+        bind:closeModal={closeModal}
+		showModal={crudDataIndex !== null}
+		title="Remove a warning"
+		on:close={resetCrudData}
+        confirmationButtons={true}
+        on:confirmation={() => {
+            crudRequest(`gaia/warning`, 'delete', {id: crudDataIndex});
+        }}
+    >
+        Are you sure you want to remove the warning '{$warnings[crudDataIndex] ? $warnings[crudDataIndex]['title']: ""}'?
+    </Modal>
+{:else}
+    No warnings
+{/if}

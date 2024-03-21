d600247754cebc9822626290dd31e53ad831469c
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-03-21 21:17:21 +0100

    `currentUser` store is now a basic store

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,10 +1,9 @@
 import { writable } from 'svelte/store';
 
-import { cookieStore } from '$lib/cookieStore.js';
 import { User } from '$lib/utils/factories.js';
 
 export const calendarEvents = writable([]);
-export const currentUser = cookieStore('userDataCache', User());
+export const currentUser = writable(User());
 export const ecosystems = writable({});
 export const ecosystemsIds = writable([]);
 export const ecosystemsActuatorData = writable({});

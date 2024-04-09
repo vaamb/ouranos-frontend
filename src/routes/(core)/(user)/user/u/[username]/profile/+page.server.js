7668c65c7eccb5be6a4582e76021620c75b91773
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-04-09 23:03:01 +0200

    Allow regular users to only see their user's info page

---
+++ src/routes/(core)/(user)/user/u/[username]/profile/+page.server.js
@@ -0,0 +1,15 @@
+import { redirect } from '@sveltejs/kit';
+
+import { User } from "$lib/utils/factories.js";
+import {permissions} from "$lib/utils/consts.js";
+
+export async function load({ parent, url }) {
+    const data = await parent();
+    const currentUser = User(data.userData)
+    let username = url.pathname
+    username = username.replace("/user/u/", "")
+    username = username.replace("/profile", "")
+    if (currentUser['username'] !== username && !currentUser.can(permissions.ADMIN)) {
+        throw redirect(307, '/')
+    }
+}

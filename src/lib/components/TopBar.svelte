vaamb/ouranos-frontend

commit 1311dbdfc99dd2cf65d0796e5be369c43d8e59d6

Display the connection status in the `TopBar` (#102)
* Add the store `pingServerIsConnected`

* Use `pingServerIsConnected`

* Move the ping server status indicator from the "Server info" box in the home page to the `TopBar`

* Use `Prettier`

* Simplify the root layout divs

* Simplify the `TopBar` layout divs

* Nits

src/app.html
@@ -14,7 +14,7 @@
 		%sveltekit.head%
 	</head>
 	<body data-sveltekit-preload-data="tap" style="height: 100vh; width: 100vw">
-		<div style="display: contents">
+		<div style="/*display: contents*/">
 		  	%sveltekit.body%
 		</div>
 	</body>

src/lib/components/TopBar.svelte
@@ -1,29 +1,37 @@
 <script>
 	import Fa from 'svelte-fa';
-	import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+	import { faCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
 	import { logOut } from '$lib/actions.js';
 	import { permissions } from '$lib/utils/consts.js';
-	import { currentUser, warnings } from '$lib/store.js';
+	import { currentUser, pingServerIsConnected, warnings } from '$lib/store.js';
 
 	export let development;
+	export let menuWidth = 210;
 </script>
 
-<div class="top-bar">
+<div class="top-bar" style="--menu-width:{menuWidth}">
 	<div class="left">
-		{#if $currentUser.can(permissions.ADMIN)}
-			<div>Logged as Admin</div>
-		{:else if $currentUser.can(permissions.OPERATE)}
-			<div>Logged as Operator</div>
-		{/if}
+		<div style="margin-right: 10px">
+			<Fa icon={faCircle} class={$pingServerIsConnected ? 'on' : 'deco'} />
+		</div>
+		<div class="user-permission">
+			{#if $currentUser.can(permissions.ADMIN)}
+				<div>Logged as Admin</div>
+			{:else if $currentUser.can(permissions.OPERATE)}
+				<div>Logged as Operator</div>
+			{/if}
+		</div>
 	</div>
 	<div class="center">
-		{#if development}
-			<div style="color: var(--red)">Development mode</div>
-		{/if}
+		<div class="development-warning">
+			{#if development}
+				<div class="small-screen">Dev mode</div>
+				<div class="big-screen">Development mode</div>
+			{/if}
+		</div>
 	</div>
 	<div class="right">
-		<div style="margin: auto" />
 		<div class="dropdown">
 			{#if $currentUser.isAnonymous}
 				<a href="/auth/login">
@@ -70,17 +78,31 @@
 	}
 
 	.left {
-		width: 250px;
+		display: flex;
 		margin: auto 0 auto 20px;
+	}
+
+	.user-permission {
 		display: none;
 	}
 
 	.center {
-		margin: auto auto auto 20px;
+		margin: auto;
+	}
+
+	.development-warning {
+		color: var(--red);
+	}
+
+	.small-screen {
+		display: inherit;
+	}
+
+	.big-screen {
+		display: none;
 	}
 
 	.right {
-		width: 150px;
 		display: flex;
 	}
 
@@ -89,7 +111,7 @@
 	}
 
 	.dropdown {
-		width: 55px;
+		width: 104px;
 		height: 100%;
 		justify-content: center;
 		display: block;
@@ -109,16 +131,16 @@
 		position: absolute;
 		color: var(--derived-50);
 		background-color: var(--main-95);
-		width: 113px;
+		width: 163px; /* dropdown width (104px) + padding (2 * 30px) - border (1px) */
 		z-index: 10;
 		right: 0;
-		border: thin solid var(--main-40);
+		border: 1px solid var(--main-40);
 		margin-top: 26px;
 	}
 
 	.dropdown-content > div {
 		padding: 0 16px;
-		width: 81px;
+		width: 131px /* dropdown-content (163) - padding (2 * 16px) */;
 		display: block;
 		line-height: 45px;
 	}
@@ -141,16 +163,27 @@
 	}
 
 	@media only screen and (min-width: 992px) {
+		.top-bar {
+			position: sticky;
+			top: 0;
+			width: calc(100% - var(--menu-width) * 1px);
+			margin-left: calc(var(--menu-width) * 1px);
+		}
+
 		.left {
+			width: 210px;
+		}
+
+		.user-permission {
 			display: inherit;
 		}
 
-		.center {
-			margin: auto;
+		.small-screen {
+			display: none;
 		}
 
-		.right {
-			width: 250px;
+		.big-screen {
+			display: inherit;
 		}
 
 		.warning-button {

src/lib/components/menu/Menu.svelte
@@ -106,6 +106,7 @@
 	}
 
 	nav {
+		top: 0;
 		background-color: var(--main-25);
 		color: var(--main-95);
 		display: flex;

src/lib/store.js
@@ -20,6 +20,7 @@ export const servers = writable({});
 export const serversIds = writable([]);
 export const serversCurrentData = writable({});
 export const serversHistoricData = writable({});
+export const pingServerIsConnected = writable(true);
 export const pingServerLastSeen = writable(new Date(0));
 export const pingServerLatency = writable(null);
 export const services = writable();

src/lib/utils/functions.js
@@ -119,14 +119,6 @@ export const getWeatherIcon = function (weather) {
 	return weatherIconTranslation[weather];
 };
 
-export const computePingServerStatusClass = function (pingServerLastSeen, now) {
-	if (now - pingServerLastSeen < 60 * 1000) {
-		return 'on';
-	} else {
-		return 'off';
-	}
-};
-
 export const computeServerUptime = function (serverStartTime, now) {
 	if (serverStartTime) {
 		return humanizeDuration(now - serverStartTime, {

src/routes/(core)/(open)/+page.svelte
@@ -19,7 +19,7 @@
 		ecosystemsManagement,
 		ecosystemsSensorsDataCurrent,
 		ecosystemsSensorsSkeleton,
-		pingServerLastSeen,
+		pingServerIsConnected,
 		pingServerLatency,
 		servers,
 		serversCurrentData,
@@ -32,7 +32,6 @@
 	import {
 		computeEcosystemStatusClass,
 		computeLightingHours,
-		computePingServerStatusClass,
 		computeServerUptime,
 		isEmpty,
 		ecosystemIsConnected,
@@ -202,11 +201,7 @@
 		</Box>
 	{/if}
 	{#if $currentUser.can(permissions.ADMIN)}
-		<Box
-			title="Server info"
-			align="center"
-			status={computePingServerStatusClass($pingServerLastSeen, now)}
-		>
+		<Box title="Server info" align="center">
 			<BoxItem title="Average latency">
 				{#if $pingServerLatency === null}
 					<p>Computing ...</p>

src/routes/(core)/+layout.svelte
@@ -13,12 +13,15 @@
 		engines,
 		enginesIds,
 		flashMessage,
+		pingServerIsConnected,
+		pingServerLastSeen,
 		servers,
 		serversIds,
 		services,
 		warnings
 	} from '$lib/store.js';
 	import { APP_MODE } from '$lib/utils/consts.js';
+	import { onDestroy, onMount } from 'svelte';
 
 	export let data;
 
@@ -94,6 +97,23 @@
 		msgs.shift();
 		flashMessage.set(msgs);
 	};
+
+	const updatePingServerIsConnected = function () {
+		if (new Date() - $pingServerLastSeen < 30 * 1000) {
+			pingServerIsConnected.set(true);
+		} else {
+			pingServerIsConnected.set(false);
+		}
+	};
+	let updatePingServerIsConnectedInterval = undefined;
+
+	onMount(async () => {
+		updatePingServerIsConnectedInterval = setInterval(updatePingServerIsConnected, 5 * 1000);
+	});
+
+	onDestroy(async () => {
+		clearInterval(updatePingServerIsConnectedInterval);
+	});
 </script>
 
 <Modal
@@ -106,39 +126,22 @@
 </Modal>
 
 <Menu items={menuItems} width={menuWidth} />
-<div class="container" style="--margin-width:{menuWidth}">
-	<TopBar development={appMode === APP_MODE.development} />
-	<div class="main">
-		<slot />
-	</div>
+<TopBar development={appMode === APP_MODE.development} {menuWidth} />
+<div class="main" style="--margin-width:{menuWidth}">
+	<slot />
 </div>
 
 <style>
-	.container {
-		position: absolute;
-		left: 0;
-		right: 0;
-		margin-left: 0;
-		display: flex;
-		flex-direction: column;
-		background-color: var(--main-95);
-	}
-
 	.main {
-		min-height: calc(
-			100vh - 141px
-		); /* 141px = Nav bar (65) + Top bar (45) + border (1) + padding (10+20) */
+		min-height: calc(100vh - 141px); /* 141px = Nav bar (65) + Top bar (45) + border (1) + padding (10+20) */
 		padding: 10px 20px 20px 20px;
-		position: relative;
 	}
 
 	/* Large devices (laptops/desktops, 992px and up) */
 	@media only screen and (min-width: 992px) {
-		.container {
-			margin-left: calc(var(--margin-width) * 1px);
-		}
-
 		.main {
+			margin-top: 46px; /* Top bar (45) + border (1) */
+			margin-left: calc(var(--margin-width) * 1px);
 			min-height: calc(100vh - 76px); /* 76px = Top bar (45) + border (1) + padding (10+20) */
 			padding-right: 45px;
 		}

commit c87f8380ec9e713276fe4e8dc4cd5b7947bb688f

Fix styling issues introduced by #102

src/app.html
@@ -14,7 +14,7 @@
 		%sveltekit.head%
 	</head>
 	<body data-sveltekit-preload-data="tap" style="height: 100vh; width: 100vw">
-		<div style="/*display: contents*/">
+		<div style="display: contents">
 		  	%sveltekit.body%
 		</div>
 	</body>

src/lib/components/TopBar.svelte
@@ -35,7 +35,11 @@
 		<div class="dropdown">
 			{#if $currentUser.isAnonymous}
 				<a href="/auth/login">
-					<div class="center-content">Log in</div>
+					<div class="center-content" style="display: flex">
+						<div style="margin: auto">
+							Log in
+						</div>
+					</div>
 				</a>
 			{:else}
 				<button class="reset-button dropdown-button center-content">
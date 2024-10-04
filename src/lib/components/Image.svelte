062529a18b48a8dcd50207034a6096e29ee660ce
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-10-04 12:48:06 +0200

    Add a basic `Image` component

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,86 @@
+<script>
+	import { onMount } from 'svelte';
+	import Fa from 'svelte-fa';
+	import { faNotdef, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+	export let source;
+	export let width;
+	export let height;
+	export let alt;
+	export let caption;
+
+	let image = new Image(); // bound to the image container
+	let loading = false;
+	let loaded = false;
+	let error = false;
+
+	onMount(() => {
+		loading = true;
+
+		image.src = source;
+		image.alt = alt;
+
+		image.onload = () => {
+			loading = false;
+			loaded = true;
+		};
+
+		image.onerror = () => {
+			loading = false;
+			error = true;
+		};
+	});
+</script>
+
+<figure style="--image-height: {height}; --image-width:{width}">
+	<div class="image-wrapper" style="display: {!loaded ? 'none' : ''}">
+		<img bind:this={image} style="display: {loaded}" />
+		{#if caption}
+			<figcaption>
+				{caption}
+			</figcaption>
+		{/if}
+	</div>
+
+	{#if loading}
+		<div class="image-icon">
+			<Fa icon={faSpinner} spin size="3x" />
+		</div>
+	{:else if error}
+		<div class="image-icon">
+			<Fa icon={faNotdef} size="3x" />
+		</div>
+	{/if}
+</figure>
+
+<style>
+	figure {
+		height: calc(var(--image-height) * 1px);
+		max-width: calc(var(--image-width) * 1px);
+		position: relative;
+		display: flex;
+		padding: 1px;
+		border: thin var(--derived-50) solid;
+	}
+
+	img {
+		height: 100%;
+		width: 100%;
+		object-fit: cover;
+		margin: auto;
+	}
+
+	figcaption {
+		position: absolute;
+		width: calc(100% - 8px); /* paddings */
+		bottom: 1px;
+		padding: 3px;
+		background-color: rgba(0, 0, 0, 35%);
+		color: var(--gray-90);
+		font-size: small;
+	}
+
+	.image-icon {
+		margin: auto;
+	}
+</style>

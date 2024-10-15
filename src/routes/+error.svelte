07da5170c8a6934336f2f32dd5999e9634db503a
Valentin Ambroise <valentin.ambroise@outlook.com>
2024-10-15 21:51:46 +0200

    Center error message

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="center-content">
+    <div style="max-width: 80%">
+        <h1>{$page.status}</h1>
+        <p>{$page.error.message}</p>
+    </div>
+</div>

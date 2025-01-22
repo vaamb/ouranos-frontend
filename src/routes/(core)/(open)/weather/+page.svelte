b85a5d66d96a5f416f4d7db51827fd8be149c110
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-01-22 23:27:48 +0100

    Increase the data displayed in the weather page

--- src/routes/(core)/(open)/weather/+page.svelte
+++ src/routes/(core)/(open)/weather/+page.svelte
@@ -34,10 +34,10 @@
 		};
 	};
 
-	const getHourlyDataset = function (weatherHourly, measure) {
+	const getHourlyDataset = function (weatherHourly, measure, timeScale=23) {
 		return {
 			label: capitalize(measure.replace('_', ' ')),
-			data: weatherHourly.slice(0, 23).map((elem) => elem[measure]),
+			data: weatherHourly.slice(0, timeScale).map((elem) => elem[measure]),
 			borderColor: {
 				temperature: colors.red,
 				humidity: colors.blue,
@@ -79,7 +79,9 @@
 			<h1>{capitalize($weatherCurrently['summary'])}</h1>
 			<p>Temperature: {$weatherCurrently['temperature'].toFixed(1)} °C</p>
 			<p>Humidity: {$weatherCurrently['humidity'].toFixed(1)} %</p>
-			<!--<p>Precipitation: {$weatherCurrently['precipitation_probability'].toFixed(1)} %</p>-->
+			{#if !isEmpty($weatherHourly)}
+				<p>Precipitation: {$weatherHourly[0]['precipitation_probability'].toFixed(1)} %</p>
+			{/if}
 			<p>Wind: {$weatherCurrently['wind_speed'].toFixed(1)} km/h</p>
 			<p>Cloud cover: {$weatherCurrently['cloud_cover'].toFixed(1)} %</p>
 		{:else}
@@ -106,8 +108,8 @@
 			</div>
 			<Graph
 				height="200px"
-				datasets={[getHourlyDataset($weatherHourly, currentMeasure)]}
-				labels={$weatherHourly.slice(0, 23).map((elem) => new Date(elem['timestamp']))}
+				datasets={[getHourlyDataset($weatherHourly, currentMeasure, 47)]}
+				labels={$weatherHourly.slice(0, 47).map((elem) => new Date(elem['timestamp']))}
 				suggestedMin={getSuggestedMin(currentMeasure)}
 				suggestedMax={getSuggestedMax(currentMeasure)}
 			/>

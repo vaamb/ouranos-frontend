570821895cf374ee17d1bbf33fa50e97ec0e6e1a
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-02-03 20:46:51 +0100

    Nits

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,7 +8,7 @@
 		<h1>{$page.status}</h1>
 		<p>{$page.error.message}</p>
 		<button
-				class="submit-button"
+			class="submit-button"
 			onclick={() => {
 				if (browser) window.history.back();
 			}}

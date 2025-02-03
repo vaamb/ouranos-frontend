bd00ed51ef60f852a79890c5a356de3bb01cad99
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-02-03 23:23:01 +0100

    WIP

--- src/routes/(core)/(open)/wiki/u/[topic]/u/[article]/+page.svelte
+++ src/routes/(core)/(open)/wiki/u/[topic]/u/[article]/+page.svelte
@@ -1,11 +1,16 @@
 <script>
 	import { onMount } from 'svelte';
 
+	import Fa from 'svelte-fa';
+	import { faNotdef } from '@fortawesome/free-solid-svg-icons';
+
 	import axios from 'axios';
 	import { marked } from 'marked';
 
 	import HeaderLine from '$lib/components/HeaderLine.svelte';
+	import Modal from '$lib/components/Modal.svelte';
 
+	import { fetchWikiPictures } from "$lib/actions.svelte.js";
 	import { STATIC_URL } from '$lib/utils/consts.js';
 	import { capitalize } from '$lib/utils/functions.js';
 
@@ -56,6 +61,13 @@
 	};
 	marked.use({ renderer });
 
+	let modal = $state()
+
+	let updatingContent = $state(null);
+	let parsedUpdatingContent = $derived.by(() => {
+		if (updatingContent) return marked(updatingContent);
+	});
+
 	onMount(async () => {
 		pictures = await fetchWikiPictures(article["topic"], article["name"])
 		content = await axios.get(`${STATIC_URL}/${article['path']}`).then((response) => {
@@ -65,12 +77,20 @@
 </script>
 
 <HeaderLine title={capitalize(article.name)} />
-
+<button onclick={() => updatingContent = content}>Update</button>
 <div class="text">
 	{#if content && pictures !== undefined}
 		{@html marked(content)}
 	{/if}
 </div>
+
+<Modal
+	bind:this={modal}
+	onclose={() => updatingContent = null}
+>
+	<textarea bind:value={updatingContent}></textarea>
+	{@html marked(content)}
+</Modal>
 
 <style>
 	.img {

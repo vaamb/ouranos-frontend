3fb280215a0134cd28c4c33f9a35ea37d4c662a5
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-02-03 20:45:09 +0100

    Add a "go back" button on error pages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { browser } from '$app/environment';
 	import { page } from '$app/stores';
 </script>
 
@@ -6,5 +7,13 @@
 	<div style="max-width: 80%">
 		<h1>{$page.status}</h1>
 		<p>{$page.error.message}</p>
+		<button
+				class="submit-button"
+			onclick={() => {
+				if (browser) window.history.back();
+			}}
+		>
+			Go back
+		</button>
 	</div>
 </div>

bc37d6eae61a1c01601ad45e359bed3728db13e5
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2025-04-09 23:31:20 +0200

    Display actuator boxes if there is recent data or if the actuator is currently active (#175)
    * Display actuator boxes if there is recent data or if the actuator is currently active
    
    - Fix an issue when an actuator is active but its status and mode don't change
    
    * Reduce diff

--- src/routes/(core)/(open)/ecosystem/[ecosystem]/actuators/+page.svelte
+++ src/routes/(core)/(open)/ecosystem/[ecosystem]/actuators/+page.svelte
@@ -85,7 +85,7 @@
 {#await fetchEcosystemActuatorsState(ecosystemUID) then actuatorsState_notUsed}
 	{#each actuatorTypes as actuator}
 		{#await fetchEcosystemActuatorRecords(ecosystemUID, actuator) then ecosystemsActuatorsRecords_notUsed}
-			{#if hasBeenActive($ecosystemsActuatorsRecords[getStoreDataKey(ecosystemUID, actuator)])}
+			{#if $ecosystemsActuatorsState[ecosystemUID][actuator]['active'] || hasBeenActive($ecosystemsActuatorsRecords[getStoreDataKey(ecosystemUID, actuator)])}
 				{@const actuatorRecords = $ecosystemsActuatorsRecords[getStoreDataKey(ecosystemUID, actuator)]}
 				{@const drawGraph = actuatorRecords.values.length >= 3}
 				<Box title={capitalize(actuator)} direction="row" maxWidth={drawGraph ? null : '325px'}>

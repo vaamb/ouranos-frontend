0a4ebfe592540d79d24f73457d273e2807cd692e
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-04-09 18:59:01 +0200

    Limit user's profile page access to the user and admins only

--- src/routes/(core)/(user)/user/[username]/profile/+page.svelte
+++ src/routes/(core)/(user)/user/[username]/profile/+page.svelte
@@ -1,6 +1,4 @@
 <script>
-	import { page } from '$app/stores';
-
 	import Fa from 'svelte-fa';
 	import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,11 +6,11 @@
 	import HeaderLine from '$lib/components/HeaderLine.svelte';
 	import Modal from '$lib/components/Modal.svelte';
 
-	import { crudRequest, fetchUserDescription } from '$lib/actions.svelte.js';
-	import { currentUser } from '$lib/store.svelte.js';
+	import { crudRequest } from '$lib/actions.svelte.js';
 	import { capitalize, getStatusClass, formatDateTime } from '$lib/utils/functions.js';
 
-	let username = $derived($page['params']['username']);
+	let { data } = $props();
+	let userDescription = data.userDescription;
 
 	const seenLastly = function (userDescription) {
 		if (!userDescription['last_seen']) {
@@ -38,125 +36,126 @@
 	};
 </script>
 
-<HeaderLine title="{username}'s profile" />
-{#await fetchUserDescription(username) then userDescription}
-	<table class="table-base table-narrow">
-		<tbody>
-			<tr>
-				<td>Username</td>
-				<td>
-					{userDescription['username']} &nbsp;
-					<Fa icon={faCircle} class={getStatusClass(seenLastly(userDescription))} />
-				</td>
-			</tr>
-			<tr>
-				<td>Firstname</td>
-				<td>{emptyIfNull(userDescription['firstname'])}</td>
-			</tr>
-			<tr>
-				<td>Lastname</td>
-				<td>{emptyIfNull(userDescription['lastname'])}</td>
-			</tr>
-			<tr>
-				<td>Role</td>
-				<td>{capitalize(userDescription['role_name'])}</td>
-			</tr>
-			<tr>
-				<td>E-mail</td>
-				<td>{userDescription['email']}</td>
-			</tr>
-			<tr>
-				<td>Registration date</td>
-				<td>{formatDateTime(new Date(userDescription['registration_datetime']))}</td>
-			</tr>
-		</tbody>
-		<tbody>
-			<tr>
-				<td colspan="2" style="text-align: center; vertical-align: middle">
-					<button
-						class="text-button"
-						onclick={() => {
-							setCrudAction('update');
-						}}
-					>
-						Update profile
-					</button>
-				</td>
-			</tr>
-			{#if !userDescription['confirmed']}
-				<tr>
-					<td colspan="2" style="text-align: center; vertical-align: middle">
-						<button
-							class="text-button"
-							onclick={() => {
-								setCrudAction('confirm');
-							}}
-						>
-							Confirm account
-						</button>
-					</td>
-				</tr>
-			{/if}
+<HeaderLine title="{userDescription['username']}'s profile" />
+<table class="table-base table-narrow">
+	<tbody>
+		<tr>
+			<td>Username</td>
+			<td>
+				{userDescription['username']} &nbsp;
+				<Fa icon={faCircle} class={getStatusClass(seenLastly(userDescription))} />
+			</td>
+		</tr>
+		<tr>
+			<td>Firstname</td>
+			<td>{emptyIfNull(userDescription['firstname'])}</td>
+		</tr>
+		<tr>
+			<td>Lastname</td>
+			<td>{emptyIfNull(userDescription['lastname'])}</td>
+		</tr>
+		<tr>
+			<td>Role</td>
+			<td>{capitalize(userDescription['role_name'])}</td>
+		</tr>
+		<tr>
+			<td>E-mail</td>
+			<td>{userDescription['email']}</td>
+		</tr>
+		<tr>
+			<td>Registration date</td>
+			<td>{formatDateTime(new Date(userDescription['created_at']))}</td>
+		</tr>
+	</tbody>
+	<tbody>
+		<tr>
+			<td colspan="2" style="text-align: center; vertical-align: middle">
+				<button
+					class="text-button"
+					onclick={() => {
+						setCrudAction('update');
+					}}
+				>
+					Update profile
+				</button>
+			</td>
+		</tr>
+		{#if !userDescription['confirmed_at']}
 			<tr>
 				<td colspan="2" style="text-align: center; vertical-align: middle">
 					<button
 						class="text-button"
 						onclick={() => {
-							setCrudAction('delete');
+							setCrudAction('confirm');
 						}}
 					>
-						Delete account
+						Confirm account
 					</button>
 				</td>
 			</tr>
-		</tbody>
-	</table>
-	<Modal
-		bind:this={modals['update']}
-		showModal={crudAction === 'update'}
-		on:close={resetCrudAction}
-		title="Update {$currentUser['username']}'s user info"
-	>
-		<Form
-			data={[
-				{ label: 'Username', key: 'username', value: userDescription['username'], disabled: true },
-				{ label: 'Firstname', key: 'firstname', value: userDescription['firstname'] },
-				{ label: 'Lastname', key: 'lastname', value: userDescription['lastname'] },
-				{ label: 'Role', key: 'role_name', value: userDescription['role_name'], disabled: true },
-				{ label: 'E-mail', key: 'email', value: userDescription['email'], disabled: true }
-			]}
-			on:confirm={(event) => {
-				const payload = event.detail;
-				crudRequest(`user/u/${$currentUser['username']}`, 'update', payload);
+		{/if}
+		<tr>
+			<td colspan="2" style="text-align: center; vertical-align: middle">
+				<button
+					class="text-button"
+					onclick={() => {
+						setCrudAction('delete');
+					}}
+				>
+					Delete account
+				</button>
+			</td>
+		</tr>
+	</tbody>
+</table>
+<Modal
+	bind:this={modals['update']}
+	showModal={crudAction === 'update'}
+	on:close={resetCrudAction}
+	title="Update {userDescription['username']}'s user info"
+>
+	<Form
+		data={[
+			{ label: 'Username', key: 'username', value: userDescription['username'], disabled: true },
+			{ label: 'Firstname', key: 'firstname', value: userDescription['firstname'] },
+			{ label: 'Lastname', key: 'lastname', value: userDescription['lastname'] },
+			{ label: 'Role', key: 'role_name', value: userDescription['role_name'], disabled: true },
+			{ label: 'E-mail', key: 'email', value: userDescription['email'], disabled: true }
+		]}
+		on:confirm={(event) => {
+			const payload = event.detail;
+			crudRequest(`user/u/${userDescription['username']}`, 'update', payload).then(() => {
 				modals['update'].closeModal();
-			}}
-			on:cancel={() => modals['update'].closeModal()}
-		/>
-	</Modal>
-	<Modal
-		bind:this={modals['confirm']}
-		showModal={crudAction === 'confirm'}
-		on:close={resetCrudAction}
-		title="Confirm {$currentUser['username']}'s account"
-		confirmationButtons={true}
-		on:confirm={() => {
-			crudRequest(`user/u/${$currentUser['username']}/confirm`, 'create');
+			});
+		}}
+		on:cancel={() => modals['update'].closeModal()}
+	/>
+</Modal>
+<Modal
+	bind:this={modals['confirm']}
+	showModal={crudAction === 'confirm'}
+	on:close={resetCrudAction}
+	title="Confirm {userDescription['username']}'s account"
+	confirmationButtons={true}
+	on:confirm={() => {
+		crudRequest(`user/u/${userDescription['username']}/confirm`, 'create').then(() => {
 			modals['confirm'].closeModal();
-		}}
-	>
-		<p>Confirm {$currentUser['username']}'s account ?</p>
-	</Modal>
-	<Modal
-		bind:this={modals['delete']}
-		showModal={crudAction === 'delete'}
-		on:close={resetCrudAction}
-		title="Delete {$currentUser['username']}'s account"
-		confirmationButtons={true}
-		on:confirm={() => {
-			crudRequest(`user/u/${$currentUser['username']}/delete`, 'create');
+		});
+	}}
+>
+	<p>Send a confirmation mail to {userDescription['username']}?</p>
+</Modal>
+<Modal
+	bind:this={modals['delete']}
+	showModal={crudAction === 'delete'}
+	on:close={resetCrudAction}
+	title="Delete {userDescription['username']}'s account"
+	confirmationButtons={true}
+	on:confirm={() => {
+		crudRequest(`user/u/${userDescription['username']}/delete`, 'create').then(() => {
 			modals['delete'].closeModal();
-		}}
-	>
-		<p>Are you sure you want to delete {$currentUser['username']}'s account ?</p>
-	</Modal>
-{/await}
+		});
+	}}
+>
+	<p>Are you sure you want to delete {userDescription['username']}'s account ?</p>
+</Modal>

0d2b049edaa32ab67944759fa3d202eb5f704f77
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-04-11 19:52:25 +0200

    Fix centering issue with the auth layout

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -2,8 +2,15 @@
 	let { children } = $props();
 </script>
 
-<div class="center-content">
+<div class="main center-content">
 	<div style="max-width: 80%">
 		{@render children?.()}
 	</div>
 </div>
+
+<style>
+	.main {
+		height: 100vh;
+		width: 100vw;
+	}
+</style>

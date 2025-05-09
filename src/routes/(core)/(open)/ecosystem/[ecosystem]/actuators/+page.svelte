cb5cf647df6bd3eac37f37d223244a678ff47e8b
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-05-09 23:47:11 +0200

    Move more of the logic of `getFormattedRecords` into `patchedActuatorsRecords`

--- src/routes/(core)/(open)/ecosystem/[ecosystem]/actuators/+page.svelte
+++ src/routes/(core)/(open)/ecosystem/[ecosystem]/actuators/+page.svelte
@@ -12,9 +12,7 @@
 		fetchEcosystemActuatorsState,
 		updateActuatorMode
 	} from '$lib/actions.svelte.js';
-	import {
-		ecosystemsActuatorsState,
-	} from '$lib/store.svelte.js';
+	import { ecosystemsActuatorsState } from '$lib/store.svelte.js';
 
 	import { actuatorTypes } from '$lib/utils/consts.js';
 	import { capitalize } from '$lib/utils/functions.js';
@@ -26,24 +24,46 @@
 	let ecosystemUID = data['ecosystemUID'];
 
 	let actuatorsRecords = $state({});
-	let patchedActuatorsRecords = $derived.by(() => {
-		let actuatorsRecordsCopy = $state.snapshot(actuatorsRecords);
+	let formattedActuatorsRecords = $derived.by(() => {
+		const endPoint = Date.now();
+		const startPoint = Date.now() - 1000 * 60 * 60 * 24 * 7;
+		let formattedRecords = {}
 		for (const actuatorType of actuatorTypes) {
-			if (
-				actuatorsRecordsCopy[actuatorType] &&
-				actuatorsRecordsCopy[actuatorType]['values'].length === 0
-			) {
-				const currentState = actuatorsRecordsCopy[actuatorType];
-				actuatorsRecordsCopy[actuatorType]['values'].push([
-					actuatorsRecordsCopy[actuatorType]['span'][1],
-					currentState['active'],
-					currentState['mode'],
-					currentState['status'],
-					currentState['level']
-				]);
+			if (!actuatorsRecords[actuatorType]) {
+				continue;
+			}
+			formattedRecords[actuatorType] = [];
+			if (actuatorsRecords[actuatorType]['values'].length === 0) {
+				// If there are no records, add start and end points only
+				formattedRecords[actuatorType].push(
+					// TODO: use current state values but don't refresh when they change
+					[startPoint, false, 'automatic', false, 0],
+					[endPoint, false, 'automatic', false, 0]
+				);
+			} else {
+				// If there are records, first add start point ...
+				let previousRecord = actuatorsRecords[actuatorType]['values'][0];
+				formattedRecords[actuatorType].push(
+					[startPoint, previousRecord[1], previousRecord[2], previousRecord[3], previousRecord[4]]
+				)
+				// ... then, we need to create "pre" data points and fill them with data from the
+				// previous record and a date a few microseconds before the current record ...
+				for (const record of actuatorsRecords[actuatorType]['values']) {
+					const recordDate = new Date(record[0]);
+					const previousRecordDate = new Date(recordDate - 10);
+					formattedRecords[actuatorType].push(
+						[previousRecordDate, previousRecord[1], previousRecord[2], previousRecord[3], previousRecord[4]],
+						[recordDate, record[1], record[2], record[3], record[4]]
+					)
+					previousRecord = record;
+				}
+				// ... and finally, add end point
+				formattedRecords[actuatorType].push(
+					[endPoint, previousRecord[1], previousRecord[2], previousRecord[3], previousRecord[4]]
+				);
 			}
 		}
-		return actuatorsRecordsCopy;
+		return formattedRecords;
 	});
 
 	const hasBeenActive = function (actuatorsRecords) {
@@ -55,29 +75,16 @@
 		return mode === 'automatic';
 	};
 
-	const getFormattedRecords = function (actuatorType) {
-		const data = patchedActuatorsRecords[actuatorType];
-		const records = data['values'];
+	const formatRecordsForGraphs = function (records) {
 		// Pre-populate data with the first bound (so they all start at the same time)
-		const labels = [new Date(data['span'][0])];
-		const modes = [convertModeToBool(records[0][2])];
-		const statuses = [records[0][3]];
-		// Fill datasets from record data
-		// To have nice graphs, we need to create "pre" data points and fill it with data from the last record and a
-		// date a few microseconds earlier
-		let lastRecord = records[0];
+		const labels = [];
+		const modes = [];
+		const statuses = [];
 		for (const record of records) {
-			const date = new Date(record[0]);
-			const preDate = new Date(date - 5000);
-			labels.push(preDate, date);
-			modes.push(convertModeToBool(lastRecord[2]), convertModeToBool(record[2]));
-			statuses.push(lastRecord[3], record[3]);
-			lastRecord = record;
+			labels.push(record[0]);
+			modes.push(convertModeToBool(record[2]));
+			statuses.push(record[3]);
 		}
-		// Post populate data with the last bound (so they all end at the same time)
-		labels.push(new Date(data['span'][1]));
-		modes.push(convertModeToBool(lastRecord[2]));
-		statuses.push(lastRecord[3]);
 		return {
 			labels: labels,
 			datasets: [
@@ -114,8 +121,8 @@
 	{#each actuatorTypes as actuator}
 		{#if actuatorsRecords[actuator]}
 			{#if $ecosystemsActuatorsState[ecosystemUID][actuator]['active'] || hasBeenActive(actuatorsRecords[actuator])}
-				{@const actuatorRecords = actuatorsRecords[actuator]}
-				{@const drawGraph = actuatorRecords.values.length >= 3}
+				{@const formattedActuatorsRecord = formattedActuatorsRecords[actuator]}
+				{@const drawGraph = formattedActuatorsRecord.length >= 5}
 				<Box title={capitalize(actuator)} direction="row" maxWidth={drawGraph ? null : '325px'}>
 					{#if $ecosystemsActuatorsState[ecosystemUID][actuator]['active']}
 						<BoxItem maxWidth={drawGraph ? '305px' : null}>
@@ -136,10 +143,10 @@
 						</BoxItem>
 					{/if}
 					<BoxItem>
-						{@const formattedActuatorRecords = getFormattedRecords(actuator)}
+						{@const graphData = formatRecordsForGraphs(formattedActuatorsRecord)}
 						<Graph
-							datasets={formattedActuatorRecords.datasets}
-							labels={formattedActuatorRecords.labels}
+							datasets={graphData.datasets}
+							labels={graphData.labels}
 							suggestedMax="1"
 							height="200px"
 							legend={{

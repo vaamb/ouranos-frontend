547325337c61318b285129a6c168660db1213c6d
Valentin Ambroise <113367796+vaamb@users.noreply.github.com>
2025-05-10 22:55:42 +0200

    Update actuator graphs at determined interval and on data update (#197)
    * Make `ecosystemsActuatorsRecords` a rune
    
    * Simplify
    
    * Make `patchActuatorRecords` a derived state
    
    * Move more of the logic of `getFormattedRecords` into `patchedActuatorsRecords`
    
    * Update actuators graph when receiving "actuators_data" event
    
    * Prepare to allow graph to update every X mins
    
    * Fix data update
    
    * Cleanup
    
    * Prettier
    
    * Update graphs every 5 minutes

--- src/routes/(core)/(open)/ecosystem/[ecosystem]/actuators/+page.svelte
+++ src/routes/(core)/(open)/ecosystem/[ecosystem]/actuators/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { onDestroy, onMount } from 'svelte';
+
 	import Box from '$lib/components/layout/Box.svelte';
 	import BoxItem from '$lib/components/layout/BoxItem.svelte';
 	import Graph from '$lib/components/Graph.svelte';
@@ -10,12 +12,9 @@
 		fetchEcosystemActuatorsState,
 		updateActuatorMode
 	} from '$lib/actions.svelte.js';
-	import {
-		ecosystemsActuatorsRecords,
-		ecosystemsActuatorsState,
-		getStoreDataKey
-	} from '$lib/store.svelte.js';
+	import { ecosystemsActuatorsState } from '$lib/store.svelte.js';
 
+	import { socketio } from '$lib/socketio.svelte.js';
 	import { actuatorTypes } from '$lib/utils/consts.js';
 	import { capitalize } from '$lib/utils/functions.js';
 	import { colors } from '$lib/utils/styling.js';
@@ -25,53 +24,76 @@
 	let ecosystemName = data['ecosystemName'];
 	let ecosystemUID = data['ecosystemUID'];
 
-	const hasBeenActive = function (ecosystemsActuatorsRecords) {
+	let endPoint = $state(Date.now());
+	let startPoint = $derived(new Date(endPoint - 1000 * 60 * 60 * 24 * 7));
+
+	let actuatorsRecords = $state({});
+	let formatActuatorsRecords = function (actuatorRecords, actuatorState, startPoint, endPoint) {
+		let formattedRecords = [];
+		if (actuatorRecords['values'].length === 0) {
+			// If there are no records, add start and end points only
+			formattedRecords.push(
+				[startPoint, actuatorState[1], actuatorState[2], actuatorState[3], actuatorState[4]],
+				[endPoint, actuatorState[1], actuatorState[2], actuatorState[3], actuatorState[4]]
+			);
+		} else {
+			// If there are records, first add start point ...
+			let previousRecord = actuatorRecords['values'][0];
+			formattedRecords.push([
+				new Date(startPoint),
+				previousRecord[1],
+				previousRecord[2],
+				previousRecord[3],
+				previousRecord[4]
+			]);
+			// ... then, we need to create "pre" data points and fill them with data from the
+			// previous record and a date a few microseconds before the current record ...
+			for (const record of actuatorRecords['values']) {
+				const recordDate = new Date(record[0]);
+				const previousRecordDate = new Date(recordDate - 10);
+				formattedRecords.push(
+					[
+						previousRecordDate,
+						previousRecord[1],
+						previousRecord[2],
+						previousRecord[3],
+						previousRecord[4]
+					],
+					[recordDate, record[1], record[2], record[3], record[4]]
+				);
+				previousRecord = record;
+			}
+			// ... and finally, add end point
+			formattedRecords.push([
+				new Date(endPoint),
+				previousRecord[1],
+				previousRecord[2],
+				previousRecord[3],
+				previousRecord[4]
+			]);
+		}
+		return formattedRecords;
+	};
+
+	const hasBeenActive = function (actuatorsRecords) {
 		const active = (element) => element[1];
-		return ecosystemsActuatorsRecords['values'].some(active);
+		return actuatorsRecords['values'].some(active);
 	};
 
 	const convertModeToBool = function (mode) {
 		return mode === 'automatic';
 	};
 
-	const patchActuatorRecords = function (records, actuatorName) {
-		if (records['values'].length > 0) {
-			return records;
-		} else {
-			const currentState = $ecosystemsActuatorsState[ecosystemUID][actuatorName];
-			records['values'].push([
-				records['span'][1],
-				currentState['active'],
-				currentState['mode'],
-				currentState['status'],
-				currentState['level']
-			]);
-			return records;
-		}
-	};
-
-	const formatRecords = function (data) {
-		const records = data['values'];
+	const formatRecordsForGraphs = function (records) {
 		// Pre-populate data with the first bound (so they all start at the same time)
-		const labels = [new Date(data['span'][0])];
-		const modes = [convertModeToBool(records[0][2])];
-		const statuses = [records[0][3]];
-		// Fill datasets from record data
-		// To have nice graphs, we need to create "pre" data points and fill it with data from the last record and a
-		// date a few microseconds earlier
-		let lastRecord = records[0];
+		const labels = [];
+		const modes = [];
+		const statuses = [];
 		for (const record of records) {
-			const date = new Date(record[0]);
-			const preDate = new Date(date - 5000);
-			labels.push(preDate, date);
-			modes.push(convertModeToBool(lastRecord[2]), convertModeToBool(record[2]));
-			statuses.push(lastRecord[3], record[3]);
-			lastRecord = record;
+			labels.push(record[0]);
+			modes.push(convertModeToBool(record[2]));
+			statuses.push(record[3]);
 		}
-		// Post populate data with the last bound (so they all end at the same time)
-		labels.push(new Date(data['span'][1]));
-		modes.push(convertModeToBool(lastRecord[2]));
-		statuses.push(lastRecord[3]);
 		return {
 			labels: labels,
 			datasets: [
@@ -94,53 +116,91 @@
 			]
 		};
 	};
+
+	const updateActuatorsData = function (actuatorsData) {
+		for (const actuatorData of actuatorsData) {
+			if (actuatorData['ecosystem_uid'] !== ecosystemUID) {
+				continue;
+			}
+			endPoint = Date.now();
+			actuatorsRecords[actuatorData['type']]['values'].push([
+				new Date(endPoint - 100).toISOString(),
+				actuatorData['active'],
+				actuatorData['mode'],
+				actuatorData['status'],
+				actuatorData['level']
+			]);
+		}
+	};
+
+	let timeUpdate = null;
+	const updateTime = function () {
+		endPoint = Date.now();
+	};
+
+	onMount(async () => {
+		for (const actuatorType of actuatorTypes) {
+			actuatorsRecords[actuatorType] = await fetchEcosystemActuatorRecords(
+				ecosystemUID,
+				actuatorType
+			);
+		}
+		socketio.on('actuators_data', updateActuatorsData);
+		timeUpdate = setInterval(updateTime, 1000 * 60 * 5);
+	});
+
+	onDestroy(async () => {
+		socketio.off('actuators_data', updateActuatorsData);
+		clearInterval(timeUpdate);
+	});
 </script>
 
 <HeaderLine title="Actuators in {ecosystemName}" />
 
 {#await fetchEcosystemActuatorsState(ecosystemUID) then actuatorsState_notUsed}
-	{#each actuatorTypes as actuator}
-		{#await fetchEcosystemActuatorRecords(ecosystemUID, actuator) then ecosystemsActuatorsRecords_notUsed}
-			{#if $ecosystemsActuatorsState[ecosystemUID][actuator]['active'] || hasBeenActive($ecosystemsActuatorsRecords[getStoreDataKey(ecosystemUID, actuator)])}
-				{@const actuatorRecords = $ecosystemsActuatorsRecords[getStoreDataKey(ecosystemUID, actuator)]}
-				{@const drawGraph = actuatorRecords.values.length >= 3}
-				<Box title={capitalize(actuator)} direction="row" maxWidth={drawGraph ? null : '325px'}>
-					{#if $ecosystemsActuatorsState[ecosystemUID][actuator]['active']}
-						<BoxItem maxWidth={drawGraph ? '305px' : null}>
-							<Switch
-								actuatorType={actuator}
-								status={$ecosystemsActuatorsState[ecosystemUID][actuator]['status']}
-								mode={$ecosystemsActuatorsState[ecosystemUID][actuator]['mode']}
-								useTimer={true}
-								on:switch={(event) => {
-									updateActuatorMode(
-										ecosystemUID,
-										event['detail']['actuatorType'],
-										event['detail']['mode'],
-										event['detail']['countdown']
-									);
-								}}
-							/>
-						</BoxItem>
-					{/if}
-					{@const actuatorRecords =
-						$ecosystemsActuatorsRecords[getStoreDataKey(ecosystemUID, actuator)]}
-					{@const patchedActuatorRecords = patchActuatorRecords(actuatorRecords, actuator)}
-					<BoxItem>
-						{@const formattedActuatorRecords = formatRecords(patchedActuatorRecords)}
-						<Graph
-							datasets={formattedActuatorRecords.datasets}
-							labels={formattedActuatorRecords.labels}
-							suggestedMax="1"
-							height="200px"
-							legend={{
-								display: true,
-								position: 'right'
+	{#each Object.entries(actuatorsRecords) as [actuator, actuatorRecords]}
+		{@const actuatorState = $ecosystemsActuatorsState[ecosystemUID][actuator]}
+		{#if actuatorState['active'] || hasBeenActive(actuatorRecords)}
+			{@const drawGraph = actuatorRecords['values'].length >= 3}
+			<Box title={capitalize(actuator)} direction="row" maxWidth={drawGraph ? null : '325px'}>
+				{#if $ecosystemsActuatorsState[ecosystemUID][actuator]['active']}
+					<BoxItem maxWidth={drawGraph ? '305px' : null}>
+						<Switch
+							actuatorType={actuator}
+							status={$ecosystemsActuatorsState[ecosystemUID][actuator]['status']}
+							mode={$ecosystemsActuatorsState[ecosystemUID][actuator]['mode']}
+							useTimer={true}
+							on:switch={(event) => {
+								updateActuatorMode(
+									ecosystemUID,
+									event['detail']['actuatorType'],
+									event['detail']['mode'],
+									event['detail']['countdown']
+								);
 							}}
 						/>
 					</BoxItem>
-				</Box>
-			{/if}
-		{/await}
+				{/if}
+				<BoxItem>
+					{@const formattedActuatorRecords = formatActuatorsRecords(
+						actuatorRecords,
+						actuatorState,
+						startPoint,
+						endPoint
+					)}
+					{@const graphData = formatRecordsForGraphs(formattedActuatorRecords)}
+					<Graph
+						datasets={graphData.datasets}
+						labels={graphData.labels}
+						suggestedMax="1"
+						height="200px"
+						legend={{
+							display: true,
+							position: 'right'
+						}}
+					/>
+				</BoxItem>
+			</Box>
+		{/if}
 	{/each}
 {/await}

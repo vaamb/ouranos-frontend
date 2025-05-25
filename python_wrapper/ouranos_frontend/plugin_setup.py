2eaa8f16b56643f59ddb7d0202c892309086adbc
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-05-25 13:50:55 +0200

    Use `Ouranos` new `Plugin` system

--- python_wrapper/ouranos_frontend/plugin_setup.py
+++ python_wrapper/ouranos_frontend/plugin_setup.py
@@ -4,6 +4,10 @@ from ouranos_frontend.main import Frontend
 
 
 plugin = Plugin(
-    name="frontend",
     functionality=Frontend,
+    description="""Launch Ouranos' Frontend
+
+    The Frontend is a web app that allows the user to interact with Ouranos 
+    using a web interface.
+    """,
 )

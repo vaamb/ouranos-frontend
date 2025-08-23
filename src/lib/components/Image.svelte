e33481d2b3f6ad51ce6f9aa2ca558eaa33ccab4b
Valentin Ambroise <valentin.ambroise@outlook.com>
2025-08-23 23:06:50 +0200

    Allow to enlarge `Image` on click

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -4,6 +4,8 @@
 	import Fa from 'svelte-fa';
 	import { faNotdef, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
+	import Modal from '$lib/components/Modal.svelte';
+
 	let {
 		source,
 		width,
@@ -29,23 +31,28 @@
 	let loaded = $state(false);
 	let error = $state(false);
 
+	// Modal
+	let enlargePicture = $state(false)
+
 	onMount(() => {
 		loading = true;
 	});
 </script>
 
-<div class="image-wrapper" style="--image-height: {height}; --image-width:{width}">
+<div class="thumbnail-wrapper" style="--image-height: {height}; --image-width:{width}">
 	<figure>
 		<img
 			bind:this={image}
 			src={source}
 			{alt}
-		  onload={() => {loading = false; loaded = true;}}
+		  onload={() => {loading = false; loaded = true; }}
 			onerror={() => {loading = false; error = true;}}
+			onclick={() => {enlargePicture = true}}
 			style="display: {loaded ? 'inherit' : 'none'}"
+			class="thumbnail"
 		/>
 		{#if loaded && caption}
-			<figcaption>
+			<figcaption class="thumbnail-caption">
 				{caption}
 			</figcaption>
 		{/if}
@@ -61,8 +68,20 @@
 	{/if}
 </div>
 
+<Modal
+  showModal={enlargePicture === true}
+	on:close={() => {enlargePicture = false}}
+	title={alt}
+>
+	<img
+		src={source}
+		{alt}
+		class="large-image"
+	>
+</Modal>
+
 <style>
-	.image-wrapper {
+	.thumbnail-wrapper {
 		height: calc(var(--image-height) * 1px);
 		width: calc(var(--image-width) * 1px);
 		position: relative;
@@ -71,14 +90,14 @@
 		border: thin var(--derived-50) solid;
 	}
 
-	img {
+	.thumbnail {
 		height: 100%;
 		width: 100%;
 		object-fit: cover;
 		margin: auto;
 	}
 
-	figcaption {
+	.thumbnail-caption {
 		position: absolute;
 		width: calc(100% - 8px); /* paddings */
 		bottom: 1px;
@@ -90,5 +109,14 @@
 
 	.image-icon {
 		margin: auto;
+	}
+
+	.large-image {
+		height: calc(95vh - 95px);  /* Modal padding: 2x20 + Title: 24+7 + Content padding: 2x10 + padding and border */
+		max-width: min(1200px, 80vw);
+		object-fit: contain;
+		margin: auto;
+		padding: 1px;
+		border: thin var(--derived-50) solid;
 	}
 </style>

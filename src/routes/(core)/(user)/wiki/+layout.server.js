9139edd74fa3b835f3ba2d1cf267283b58e94384
Valentin Ambroise <valentin.ambroise@outlook.com>
2026-04-10 20:17:54 +0200

    Add a server-side guard for the wiki pages

---
+++ src/routes/(core)/(user)/wiki/+layout.server.js
@@ -0,0 +1,12 @@
+import { redirect } from '@sveltejs/kit';
+
+export async function load({ parent }) {
+	const data = await parent();
+
+	const service = data['services'].find((service) => {
+		return service['name'] === 'wiki';
+	});
+	if (!service?.status) {
+		redirect(307, '/');
+	}
+}

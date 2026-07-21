8efbe28bd322a86218050f6976791303e651ecea
Valentin Ambroise <valentin.ambroise@outlook.com>
2026-07-21 13:47:56 +0200

    Add `ContractBanner` component that adds warning when there is a contract mismatch

---
+++ src/lib/components/ContractBanner.svelte
@@ -0,0 +1,34 @@
+<script>
+	import {
+		appState
+	} from '$lib/store.svelte.js';
+</script>
+
+{#if appState.anyContractMismatch}
+  <div class="bottom-banner">
+    <div class="center-content" style="text-align: center">
+      Frontend is out of date — everything might not work as expected
+    </div>
+  </div>
+{/if}
+
+<style>
+	.bottom-banner {
+		position: fixed;
+		bottom: 0;
+		right: 0;
+		font-size: 1rem;
+		font-weight: bold;
+		height: 50px;
+		width: 100%;
+		z-index: 10;
+    background: var(--red);
+    color:  hsl(9, 85%, 20%);
+	}
+
+  @media only screen and (min-width: 992px) {
+		.bottom-banner {
+			width: inherit;
+		}
+	}
+</style>

4ced7b4cffb4a9fbf83b733344e81786a5c36657
Valentin Ambroise <valentin.ambroise@outlook.com>
2026-08-10 22:09:48 +0200

    Document the use of `fix.js`

--- fix.js
+++ fix.js
@@ -1,4 +1,6 @@
 // eslint-disable
+// Not used at runtime: this file only exists so that PyCharm resolves the `$lib`
+// alias in imports. The real alias comes from SvelteKit / `tsconfig.json`.
 System.config({
     "paths": {
         "$lib/*": "./src/lib/*",
